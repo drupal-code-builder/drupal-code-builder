6cd07e91e1bc7b079a78458dcb30f1c6fa2f3205
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2010-07-10 22:36:33 +0000

    by joachim: Added all files from 6--2 branch to bring HEAD up to date ahead of work for 7.

---
+++ hooks/module_builder.php
@@ -0,0 +1,63 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * These are the hooks that are invoked by the Module builder.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Provide information about hook definition files to Module builder.
+ * This hook should go in a MODULE.module_builder.inc file in your module folder.
+ * Is it only loaded by Module builder when the user goes to download new hook data.
+ *
+ * @return
+ *   An array of data, keyed by module name.
+ *   The subsequent array should specify:
+ *    - url: a general url to fetch files from.
+ *      Use tokens to insert filenames and branch: %file, %branch 
+ *    - branch: the current branch of the module, eg DRUPAL-6--1, HEAD.
+ *      TODO: find a neat way to grab this with a CVS id token?
+ *    - group: the UI group these hooks should go in. This should probably be the
+ *      name of your module, but you can use '#filenames' to specify that each
+ *      of your files should form a group.
+ *      Eg 'core.php' goes in the group 'core'.
+ *    - hook_files: an array of files to fetch. The filename is the key
+ *      and the value is the file where the hook code should eventually be stored.
+ *      Use the token %module for the module machine name.
+ *      Usually this will be '%module.module' but for instance,
+ *      'install.php' has hooks that should go in '%module.install'.
+ */
+function hook_module_builder_info() {
+  $data = array(
+    // Hooks on behalf of Drupal core.
+    'system' => array(
+      'url' => 'http://cvs.drupal.org/viewvc.py/drupal/contributions/docs/developer/hooks/%file?view=co&pathrev=%branch',
+      'branch' => 'DRUPAL-6--1',
+      'group' => '#filenames',
+      'hook_files' => array(
+        // List of files we should slurp from the url for hook defs.
+        // and the destination file for processed code.
+        'core.php' =>    '%module.module',
+        'node.php' =>    '%module.module',      
+        'install.php' => '%module.install',      
+      ),
+    ),
+    // We need to do our own stuff now we have a hook!
+    'module_builder' => array(
+      'url' => 'http://cvs.drupal.org/viewvc.py/drupal/contributions/modules/module_builder/hooks/%file?view=co&pathrev=%branch',
+      'branch' => 'DRUPAL-6--2',
+      'group' => 'module builder',      
+      'hook_files' => array(
+        'module_builder.php' => '%module.module_builder.inc',
+      ),
+    ),
+  );
+  
+  return $data;
+}
---
+++ theme/module_builder.js
@@ -0,0 +1,47 @@
+/* $Id$ */
+
+/**
+ * For a given hook grouping, selects/deselects all hooks associated with it.
+ *
+ * The bulk of this function (except the crappy hard-coded stuff ;)) was
+ * courtesy of David Carrington (Thox), with enhancements
+ * made by Steven Wittens (Unconed). Thanks a lot, guys!! :D
+ */
+function check_hooks(grouping, hooks) {
+  // Loop through the hooks
+  for (i = 0; hook = hooks[i]; i++) {
+    // Find the relevant checkbox
+    hook_groups = Array('authentication', 'core', 'node');
+    for (j = 0; group = hook_groups[j]; j++) {
+      id = 'edit-hooks-' + group + '-' + hook.replace(/_/g, '-');
+      if (document.getElementById(id)) {
+        checkbox = document.getElementById(id);
+        break;
+      }
+    }
+
+    // Set the checkbox status to the status of the clicked one
+    if (typeof checkbox.checkCount == 'undefined') {
+      checkbox.checkCount = 0;
+    }
+    checkbox.checkCount += grouping.checked ? 1 : -1;
+    checkbox.checked = checkbox.checkCount > 0;
+  }
+}
+
+/**
+ * Clears the default texts on click.
+ * Only happens on a fresh form (ie not when the user clicks our back button).
+ */
+$(document).ready(function() {
+  $('.fresh .form-text.required').click(function () {
+    $(this).attr('value', '');
+    $(this).unbind('click');
+  });
+  // If the user gets here with a tab and types, lose the click clearing.
+  $('.fresh .form-text.required').keypress(function () {
+    $(this).unbind('click'); 
+  });  
+});
+  
+
