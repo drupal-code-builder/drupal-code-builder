9d7f3a019923e25962603a446b0933ab858541ad
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2013-07-26 07:27:51 +0100

    Added docs for use as a library.

--- Environment/Environment.php
+++ Environment/Environment.php
@@ -220,6 +220,40 @@ class ModuleBuilderEnvironmentDrupalUI extends ModuleBuilderEnvironmentBase {
 
 /**
  * Environment class for use as a Drupal Library.
+ *
+ * This allows use of Module Builder in the following way:
+ *  - place the Module Builder folder in sites/all/libraries
+ *  - create a normal Drupal module, with a dependency on Libraries API.
+ *
+ * Sample code to include MB as a library:
+ * @code
+ *  function YOURMODULE_libraries_info() {
+ *    $libraries['module_builder'] = array(
+ *      // Only used in administrative UI of Libraries API.
+ *      'name' => 'Module Builder Core',
+ *      'vendor url' => 'http://example.com',
+ *      'download url' => 'http://example.com/download',
+ *      // We have to declare a version.
+ *      'version' => 'none',
+ *      'files' => array(
+ *        'php' => array(
+ *          'includes/common.inc',
+ *          'ModuleBuilderFactory.php',
+ *        ),
+ *      ),
+ *      // Auto-load the files.
+ *      'integration files' => array(
+ *        'module_builder_ui' => array(
+ *          'php' => array(
+ *            'includes/common.inc',
+ *            'ModuleBuilderFactory.php',
+ *          ),
+ *        ),
+ *      ),
+ *    );
+ *    return $libraries;
+ *  }
+ * @endcode
  */
 class ModuleBuilderEnvironmentDrupalLibrary extends ModuleBuilderEnvironmentDrupalUI {
 
