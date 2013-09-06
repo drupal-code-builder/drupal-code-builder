fb006e4ae4634ac01271f0bddeada6d9a32808d3
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2013-09-06 21:48:42 +0100

    Removed methods from File generator that are now in PHPFile.

--- Generator/File.php
+++ Generator/File.php
@@ -49,87 +49,22 @@ abstract class File extends BaseGenerator {
   /**
    * Build the code files.
    *
-   * Subclasses should implement this to add their file data to the list.
+   * Subclasses should override this to add their file data to the list.
    */
   function collectFiles(&$files) {
-  }
-
-  /**
-   * Return the contents of the file.
-   *
-   * @return
-   *  An array of text strings, in the correct order for concatenation.
-   */
-  function file_contents() {
-    // If only bare code is requested, only output the body, wthout headers
-    // or footer.
-    $module_data = $this->base_component->component_data;
-    if (!empty($module_data['bare_code'])) {
-      return $this->code_body();
-    }
-
-    // File contents are built up.
-    $file_contents = array_merge(
-      array(
-        $this->file_header(),
-        $this->code_header(),
-      ),
-      // The code body is itself an array.
-      $this->code_body(),
-      array(
-        $this->code_footer(),
-      )
+    /*
+    // Example:
+    $files[$this->name] = array(
+      'path' => '', // Means base folder.
+      'filename' => $this->base_component->component_data['module_root_name'] . '.info',
+      // We pass $files in to check for files containing classes.
+      'body' => $this->code_body($files),
+      // We join the info lines with linebreaks, as they (currently!) do not
+      // come with their own lineends.
+      // TODO: fix this!
+      'join_string' => "\n",
     );
-
-    // Filter out any empty elements.
-    $file_contents = array_filter($file_contents);
-    return $file_contents;
+    */
   }
-
-  /**
-   * Return the PHP file header line.
-   */
-   function file_header()  {
-     return "<?php\n";
-   }
-
-  /**
-   * Return the file doxygen header and any custom header code.
-   *
-   * Expects $this->filename to be set.
-   */
-  function code_header() {
-    $filename = $this->filename;
-    $file_description = $this->file_doc_summary();
-    $default = <<<EOT
-/**
- * @file $filename
- * $file_description
- */
-
-EOT;
-    $code = variable_get('module_builder_header', $default);
-    return $code;
-  }
-
-  /**
-   * Return the summary line for the file docblock.
-   */
-  function file_doc_summary() {
-    return "TODO: Enter file description here.";
-  }
-
-  /**
-   * Return the main body of the file code.
-   *
-   * @return
-   *  An array of chunks of text for the code body.
-   */
-  abstract function code_body();
-
-  /**
-   * Return a file footer.
-   */
-  function code_footer() {}
 
 }
