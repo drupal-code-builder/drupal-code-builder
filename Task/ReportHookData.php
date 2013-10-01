09f549f53309e91609ccf3568a2104d5f8abfe42
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2013-10-01 08:20:51 +0100

    Changed ReportHookData to be a subclass of ReportHookDataFolder.

--- Task/ReportHookData.php
+++ Task/ReportHookData.php
@@ -7,68 +7,20 @@
 
 namespace ModuleBuider\Task;
 
+// We have no autoloading for Tasks.
+include_once(dirname(__FILE__) . "/ReportHookDataFolder.php");
+
 /**
  * Task handler for reporting on hook data.
  *
  * TODO: revisit some of these and clean up names / clean up how many we have.
  */
-class ReportHookData extends Base {
+class ReportHookData extends ReportHookDataFolder {
 
   /**
    * The sanity level this task requires to operate.
    */
   protected $sanity_level = 'hook_data';
-
-  /**
-   * Get the timestamp of the last hook data upate.
-   *
-   * @return
-   *  A unix timestamp, or NULL if the hooks have never been collected.
-   */
-  public function lastUpdatedDate() {
-    $directory = $this->environment->hooks_directory;
-    $hooks_file = "$directory/hooks_processed.php";
-    if (file_exists($hooks_file)) {
-      $timestamp = filemtime($hooks_file);
-      return format_date($timestamp, 'large');
-    }
-  }
-
-  /**
-   * Get a list of all collected hook api.php files.
-   *
-   * @return
-   *  A flat array of filenames, relative to the hooks directory. If no files
-   *  are present, an empty array is returned.
-   */
-  public function listHookFiles() {
-    $directory = $this->environment->hooks_directory;
-
-    $files = array();
-
-    if (is_dir($directory)) {
-      if ($dh = opendir($directory)) {
-        while (($file = readdir($dh)) !== FALSE) {
-          // Ignore files that don't make sense to include
-          // TODO: replace all the .foo with one of the arcane PHP string checking functions
-          if (!in_array($file, array('.', '..', '.DS_Store', 'CVS', 'hooks_processed.php'))) {
-            $files[] = $file;
-          }
-        }
-        closedir($dh);
-      }
-      else {
-        drupal_set_message(t('There was an error opening the hook documentation path. Please try again.'), 'error');
-        return NULL;
-      }
-    }
-    else {
-      drupal_set_message(t('Hook documentation path is invalid. Please return to the <a href="!settings">module builder settings</a> page to try again.', array('!settings' => url('admin/settings/module_builder'))), 'error');
-      return NULL;
-    }
-
-    return $files;
-  }
 
   /**
    * Get the list of hook data.
