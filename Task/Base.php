a76744d7f9c182961c695d18aa09166fc0d962bf
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2013-10-06 17:58:26 +0100

    Added docs to Task Base class.

--- Task/Base.php
+++ Task/Base.php
@@ -9,6 +9,17 @@ namespace ModuleBuider\Task;
 
 /**
  * Base class for Tasks.
+ *
+ * Task classes contain code to perform distinct operations. For example,
+ * getting hook data by finding and processing api.php files is the domain of
+ * the Collect task.
+ *
+ * Each task may also define the sanity level it requires. This allows the
+ * Environment object to state whether the current environment is ready for the
+ * task. For example, generating a module requires hook data to be ready;
+ * whereas processing hook data does not.
+ *
+ * Task objects are instantiated by ModuleBuilderFactory::getTask().
  */
 class Base {
 
