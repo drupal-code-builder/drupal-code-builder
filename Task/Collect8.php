dc31bacdd0ea08c65edcdf5a0ccdd3eb695e6f0f
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2014-09-14 15:19:09 +0100

    Moved D8 version of invoking of our hook to Collect8 Task handler.

--- Task/Collect8.php
+++ Task/Collect8.php
@@ -80,7 +80,7 @@ class Collect8 extends Collect {
    */
   private function getHookDestinations(&$hook_files) {
     // Get data by invoking our hook.
-    $data = _module_builder_invoke_hook();
+    $data = $this->invokeInfoHook();
 
     // Incoming data is destination key, array of hooks.
     // (Because it makes typing the data out easier! Computers can just adapt.)
@@ -115,6 +115,54 @@ class Collect8 extends Collect {
     }
 
     //print_r($hook_files);
+  }
+
+  /**
+   * Invoke hook_module_builder_info().
+   */
+  function invokeInfoHook() {
+    $mb_factory = module_builder_get_factory();
+    $mb_factory->environment->loadInclude('common_version');
+    $major_version = $mb_factory->environment->major_version;
+
+    // TODO: just get ours if no bootstrap?
+    $mask = '/\.module_builder.inc$/';
+
+    // Based on change record https://www.drupal.org/node/2198695
+    $mb_files = array();
+    foreach (\Drupal::moduleHandler()->getModuleList() as $name => $module) {
+      $mb_files += file_scan_directory($module->getPath(), $mask);
+    }
+
+    $module_data = array();
+
+    foreach ($mb_files as $file) {
+      // Our system listing wrapper ensured that there is a uri property on all versions.
+      include_once($file->uri);
+      // Use a property of the (badly-documented!) $file object that is common to both D6 and D7.
+      $module = str_replace('.module_builder', '', $file->name);
+      // Note that bad data got back from the hook breaks things.
+      if ($result = module_invoke($module, 'module_builder_info', $major_version)) {
+        $module_data = array_merge($module_data, $result);
+      }
+    }
+
+    //print_r($module_data);
+
+    // If we are running as Drush command, we're not an installed module.
+    if (!Drupal::moduleHandler()->moduleExists('module_builder')) {
+      include_once(dirname(__FILE__) . '/../module_builder.module_builder.inc');
+      $result = module_builder_module_builder_info($major_version);
+      $data = array_merge($module_data, $result);
+    }
+    else {
+      $data = $module_data;
+      // Yeah we switch names so the merging above isn't affected by an empty array.
+      // Gah PHP. Am probably doin it wrong.
+    }
+
+    //drush_print_r($data);
+    return $data;
   }
 
 }
