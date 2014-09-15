b144773bed874e022ca950b98b7d8149aef34075
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2014-09-15 17:22:30 +0100

    Added docgroup for helpers.

--- Environment/Environment.php
+++ Environment/Environment.php
@@ -577,6 +577,25 @@ class ModuleBuilderEnvironmentTestsTempLocation extends ModuleBuilderEnvironment
 }
 
 /**
+ * @defgroup module_builder_environment_version_helpers Environment version helpers
+ * @{
+ * Wrapper objects for Drupal APIs that change between Drupal major versions.
+ *
+ * These allow the environment classes to work orthogonally across different
+ * environments (Drush, Drupal UI) and different core versions.
+ *
+ * Each major version of Drupal core needs a version helper class. This is
+ * instantiated by the environment object's initVersionHelper(). No direct calls
+ * should be made to the helper, rather, the environment base class should
+ * provide a wrapper.
+ *
+ * Version helper classes inherit in a cascade, with older versions inheriting
+ * from newer. This means that if, say, an API function does not change between
+ * Drupal 6 and 7, then its wrapper does not need to be present in the Drupal 6
+ * helper class.
+ */
+
+/**
  * Environment helper for Drupal 8.
  */
 class ModuleBuilderEnvironmentVersionHelper8 {
@@ -891,3 +910,7 @@ class ModuleBuilderEnvironmentVersionHelper6 {
 class ModuleBuilderEnvironmentVersionHelper5 extends ModuleBuilderEnvironmentVersionHelper6 {
   // D5 helper is the same as D6.
 }
+
+/**
+* @} End of "defgroup module_builder_environment_version_helpers".
+*/
