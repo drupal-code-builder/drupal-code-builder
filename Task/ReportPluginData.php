e36c414c47453763186764ea7ddc1df8c4438f65
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2014-09-17 22:17:20 +0100

    Added Task for reporting plugin data.

---
+++ Task/ReportPluginData.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @file
+ * Definition of ModuleBuider\Task\ReportPluginData.
+ */
+
+namespace ModuleBuider\Task;
+
+// We have no autoloading for Tasks.
+include_once(dirname(__FILE__) . "/ReportHookDataFolder.php");
+
+/**
+ * Task handler for reporting on hook data.
+ *
+ * TODO: revisit some of these and clean up names / clean up how many we have.
+ * Consider merging into a ReportComponentData Task.
+ */
+class ReportPluginData extends ReportHookDataFolder {
+
+  /**
+   * The sanity level this task requires to operate.
+   */
+  protected $sanity_level = 'hook_data';
+
+  /**
+   * Get the list of plugin data.
+   *
+   * @return
+   *  The unserialized contents of the processed plugin data file.
+   */
+  function listPluginData() {
+    // We may come here several times, so cache this.
+    // TODO: look into finer-grained caching higher up.
+    static $plugin_data;
+
+    if (isset($hook_data)) {
+      return $hook_data;
+    }
+
+    $directory = $this->environment->hooks_directory;
+
+    $plugins_file = "$directory/plugins_processed.php";
+    if (file_exists($plugins_file)) {
+      $plugins_data = unserialize(file_get_contents($plugins_file));
+      return $plugins_data;
+    }
+    // Sanity checks ensure we never get here, but in case they have been
+    // skipped, return something that makes sense to the caller.
+    return array();
+  }
+  
+  /**
+   * Get plugin types as a list of options.
+   *
+   * @return
+   *   An array of plugin types as options suitable for FormAPI.
+   */
+  function listPluginNamesOptions($style = 'full') {
+    $data = $this->listPluginData();
+
+    $return = array();
+    foreach ($data as $plugin_type_name => $plugin_type_info) {
+      $return[$plugin_type_name] = $plugin_type_name;
+    }
+
+    return $return;
+  }
+
+}
