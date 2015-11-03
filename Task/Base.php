ed5a9f30912aaf7a9386e12dde728232712b9022
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2015-11-03 15:12:25 +0000

    Added to docs.

--- Task/Base.php
+++ Task/Base.php
@@ -14,6 +14,9 @@ namespace ModuleBuider\Task;
  * getting hook data by finding and processing api.php files is the domain of
  * the Collect task.
  *
+ * Public methods on Task classes are part of Module Builder's public API, and
+ * may be considered stable.
+ *
  * Each task may also define the sanity level it requires. This allows the
  * Environment object to state whether the current environment is ready for the
  * task. For example, generating a module requires hook data to be ready;
