138c8cfef62eb862807330c2ee97d50d20bc0e4f
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2015-12-09 23:43:28 +0000

    Added replacement of file variables to YML generator.

--- Generator/YMLFile.php
+++ Generator/YMLFile.php
@@ -22,21 +22,38 @@ class YMLFile extends File {
     // be replaced.
     $this->filename = str_replace('%module', $this->base_component->component_data['root_name'], $this->name);
 
-    $yaml_parser = new \Symfony\Component\Yaml\Yaml;
-    $yaml = $yaml_parser->dump($this->component_data['yaml_data'], 2, 2);
-    //drush_print_r($yaml);
-
-    // Because the yaml is all built for us, this is just a singleton array.
-    $body = array($yaml);
-
     $files[$this->filename] = array(
       'path' => '', // Means base folder.
       'filename' => $this->filename,
-      'body' => $body,
+      'body' => $this->getYamlBody($this->component_data['yaml_data']),
       // We join code files up on a single newline. This means that each
       // component is responsible for ending its own lines.
       'join_string' => "\n",
     );
+  }
+
+  /**
+   * Get the YAML body for the file.
+   *
+   * @param $yaml_data_array
+   *  An array of data to convert to YAML.
+   *
+   * @return
+   *  An array containing the YAML string.
+   */
+  protected function getYamlBody($yaml_data_array) {
+    $yaml_parser = new \Symfony\Component\Yaml\Yaml;
+    $yaml = $yaml_parser->dump($yaml_data_array, 6, 2);
+    //drush_print_r($yaml);
+
+    // Replace variables
+    $variables = $this->getReplacements();
+    $yaml = strtr($yaml, $variables);
+
+    // Because the yaml is all built for us, this is just a singleton array.
+    $body = array($yaml);
+
+    return $body;
   }
 
 }
