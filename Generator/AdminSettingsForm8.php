780240580874963da4afd2577b357e5b9b628be8
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-02-20 21:26:52 +0000

    Added admin settings form for Drupal 8.

---
+++ Generator/AdminSettingsForm8.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @file
+ * Contains ModuleBuilder\Generator\AdminSettingsForm8.
+ */
+
+namespace ModuleBuilder\Generator;
+
+/**
+ * Component generator: admin form for modules.
+ */
+class AdminSettingsForm8 extends Form8 {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function requestedComponentHandling() {
+    return 'singleton';
+  }
+
+  /**
+   * Set properties relating to class name.
+   */
+  protected function setClassNames($component_name) {
+    // Form the full class name.
+    $class_name_pieces = array(
+      'Drupal',
+      $this->base_component->component_data['root_name'],
+      'Form',
+      'AdminSettingsForm',
+    );
+    $qualified_class_name = implode('\\', $class_name_pieces);
+
+    parent::setClassNames($qualified_class_name);
+  }
+
+  /**
+   * Return an array of subcomponent types.
+   */
+  protected function requiredComponents() {
+    $components = parent::requiredComponents();
+
+    // This takes care of adding hook_menu() and so on.
+    $form_name = $this->getFormName();
+    $components['admin/config/TODO-SECTION/%module'] = array(
+      'component_type' => 'RouterItem',
+      'title' => 'Administer %readable',
+      'description' => 'Configure settings for %readable.',
+    );
+
+    $components['Permissions'] = array(
+      'component_type' => 'Permissions',
+      'request_data' => array(
+        'administer %module',
+      ),
+    );
+
+    return $components;
+  }
+
+  /**
+   * The name of the form.
+   */
+  protected function getFormName() {
+    // TODO: this should be set in our data.
+    $root_component_data = $this->getRootComponentData();
+    $base_component_name = $root_component_data['root_name'];
+    return "{$base_component_name}_settings_form";
+  }
+
+}
---
+++ Generator/Form8.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @file
+ * Contains ModuleBuilder\Generator\Form8.
+ */
+
+namespace ModuleBuilder\Generator;
+
+/**
+ * Generator class for forms on Drupal 8.
+ */
+class Form8 extends PHPClassFile {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function requiredComponents() {
+    $form_name = $this->getFormName();
+
+    $components = array(
+      // Request the form functions.
+      'getFormId' => array(
+        'component_type' => 'PHPFunction',
+        'code_file' => $this->name,
+        'doxygen_first' => '{@inheritdoc}',
+        'declaration' => 'public function getFormId()',
+        'body' => array(
+          "return '$form_name';",
+        ),
+        'body_indent' => 2,
+      ),
+      'buildForm' => array(
+        'component_type' => 'PHPFunction',
+        'code_file' => $this->name,
+        'doxygen_first' => 'Form constructor.',
+        'declaration' => 'public function buildForm(array $form, FormStateInterface $form_state)',
+        'body' => array(
+          "£form['element] = array(",
+          "  '#type' => 'textfield',",
+          "  '#title' => t('Enter a value'),",
+          "  '#required' => TRUE,",
+          ");",
+          "",
+          "return £form;",
+        ),
+        'body_indent' => 2,
+      ),
+      'submitForm' => array(
+        'component_type' => 'PHPFunction',
+        'code_file' => $this->name,
+        'doxygen_first' => 'Form submission handler.',
+        'declaration' => "public function submitForm(array &$form, FormStateInterface $form_state)",
+        'body' => '',
+        'body_indent' => 2,
+      ),
+    );
+
+    return $components;
+  }
+
+}
