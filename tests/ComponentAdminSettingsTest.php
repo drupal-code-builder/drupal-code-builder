9fcd6fc7e1d1109179568e435ff2ecd239d17b13
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-02-23 20:47:26 +0000

    Added further tests of admin settings component.

--- tests/ComponentAdminSettingsTest.php
+++ tests/ComponentAdminSettingsTest.php
@@ -53,6 +53,7 @@ class ComponentAdminSettingsTest extends ModuleBuilderTestBase {
     $module_file = $files["$module_name.module"];
     $this->assertHookImplementation($module_file, 'hook_permission', $module_name, "The module file contains a function declaration that implements hook_permission().");
     $this->assertFunctionCode($module_file, "{$module_name}_permission", "permissions['administer $module_name']");
+    $this->assertHookImplementation($module_file, 'hook_menu', $module_name, "The module file contains a function declaration that implements hook_permission().");
 
     // Check the .info file.
     $info_file = $files["$module_name.info"];
@@ -61,6 +62,74 @@ class ComponentAdminSettingsTest extends ModuleBuilderTestBase {
     $this->assertInfoLine($info_file, 'description', $module_data['short_description'], "The info file declares the module description.");
     $this->assertInfoLine($info_file, 'core', "7.x", "The info file declares the core version.");
     $this->assertInfoLine($info_file, 'configure', "admin/config/TODO-SECTION/$module_name", "The info file declares the configuration path.");
+  }
+
+  /**
+   * Test Admin Settings component with other hooks.
+   */
+  function testAdminSettingsOtherHooksTest() {
+    $this->setupModuleBuilder(7);
+
+    // Create a module.
+    $module_name = 'testmodule';
+    $module_data = array(
+      'base' => 'module',
+      'root_name' => $module_name,
+      'readable_name' => 'Test module',
+      'short_description' => 'Test Module description',
+      'hooks' => array(
+        'init'
+      ),
+      'requested_components' => array(
+        'AdminSettingsForm' => 'AdminSettingsForm',
+        'info' => 'info',
+      ),
+      'requested_build' => array(
+      ),
+      'readme' => FALSE,
+    );
+    $files = $this->generateModuleFiles($module_data);
+
+    // Check the .module file.
+    $module_file = $files["$module_name.module"];
+    $this->assertHookImplementation($module_file, 'hook_permission', $module_name, "The module file contains a function declaration that implements hook_permission().");
+    $this->assertHookImplementation($module_file, 'hook_menu', $module_name, "The module file contains a function declaration that implements hook_permission().");
+    $this->assertHookImplementation($module_file, 'hook_init', $module_name, "The module file contains a function declaration that implements hook_permission().");
+  }
+
+  /**
+   * Test Admin Settings component with other permissions.
+   */
+  function testAdminSettingsOtherPermsTest() {
+    $this->setupModuleBuilder(7);
+
+    // Create a module.
+    $module_name = 'testmodule';
+    $module_data = array(
+      'base' => 'module',
+      'root_name' => $module_name,
+      'readable_name' => 'Test module',
+      'short_description' => 'Test Module description',
+      'hooks' => array(
+      ),
+      'permissions' => array(
+        'access testmodule',
+      ),
+      'requested_components' => array(
+        'AdminSettingsForm' => 'AdminSettingsForm',
+        'info' => 'info',
+      ),
+      'requested_build' => array(
+      ),
+      'readme' => FALSE,
+    );
+    $files = $this->generateModuleFiles($module_data);
+
+    // Check the .module file.
+    $module_file = $files["$module_name.module"];
+    $this->assertHookImplementation($module_file, 'hook_permission', $module_name, "The module file contains a function declaration that implements hook_permission().");
+    $this->assertFunctionCode($module_file, "{$module_name}_permission", "permissions['administer $module_name']");
+    $this->assertFunctionCode($module_file, "{$module_name}_permission", "permissions['access testmodule']");
   }
 
 }
