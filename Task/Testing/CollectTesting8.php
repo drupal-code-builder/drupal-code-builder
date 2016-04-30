c91931812854129f234caeda7849b69516cd4f2f
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-04-30 16:06:58 +0100

    Added subset of service definitions to testing data.

--- Task/Testing/CollectTesting8.php
+++ Task/Testing/CollectTesting8.php
@@ -60,6 +60,23 @@ class CollectTesting8 extends Collect8 {
   /**
    * {@inheritdoc}
    */
+  protected function gatherServiceDefinitions() {
+    $service_definitions = parent::gatherServiceDefinitions();
+
+    // For testing, only take a subset of service names.
+    $testing_service_ids = array(
+      'current_user' => TRUE,
+      'entity.manager' => TRUE,
+    );
+
+    $service_definitions = array_intersect_key($service_definitions, $testing_service_ids);
+
+    return $service_definitions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   protected function writeProcessedData($data, $type) {
     // Write the processed data to a file in our testing folder.
     $directory = Factory::getLibraryBaseDirectory()
