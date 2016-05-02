7b7cdd6f6da4872fc0d818e57b71af24a5e76341
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-05-02 15:15:45 +0100

    Added a controller class for generated routes.

--- Generator/RouterItem8.php
+++ Generator/RouterItem8.php
@@ -14,6 +14,31 @@ namespace DrupalCodeBuilder\Generator;
  */
 class RouterItem8 extends RouterItem {
 
+  use NameFormattingTrait;
+
+  /**
+   * Constructor method; sets the component data.
+   *
+   * The component name is taken to be the fully-qualified class name.
+   */
+  function __construct($component_name, $component_data, $generate_task, $root_generator) {
+    // Create a controller name from the route name.
+    $controller_class_name = $this->toCamel($component_name) . 'Controller';
+    $controller_qualified_class_name = implode('\\', [
+      'Drupal',
+      '%module',
+      'Controller',
+      $controller_class_name
+    ]);
+
+    $component_data += [
+      'controller_qualified_class' => $controller_qualified_class_name,
+      'controller_method' => 'content',
+    ];
+
+    parent::__construct($component_name, $component_data, $generate_task, $root_generator);
+  }
+
   /**
    * Declares the subcomponents for this component.
    *
@@ -21,13 +46,21 @@ class RouterItem8 extends RouterItem {
    *  An array of subcomponent names and types.
    */
   public function requiredComponents() {
-    return array(
-      // Each RouterItem that gets added will cause a repeat request of these
-      // components.
-      '%module.routing.yml' => array(
-        'component_type' => 'Routing',
-      ),
+    $components = [];
+
+    // Each RouterItem that gets added will cause a repeat request of these
+    // components.
+    $components['%module.routing.yml'] = array(
+      'component_type' => 'Routing',
     );
+
+    $controller_qualified_class = $this->component_data['controller_qualified_class'];
+    $components[$controller_qualified_class] = array(
+      // Add a sample build method to this.
+      'component_type' => 'PHPClassFile',
+    );
+
+    return $components;
   }
 
   /**
@@ -48,6 +81,8 @@ class RouterItem8 extends RouterItem {
       // Prepend a slash to the path for D8.
       'path' => '/' . $path,
       'defaults' => array(
+        '_controller' => '\\' . $this->component_data['controller_qualified_class'] .
+          '::' . $this->component_data['controller_method'],
         '_title' => $this->component_data['title'],
       ),
       'requirements' => array(
