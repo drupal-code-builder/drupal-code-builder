cb7d735c459e7439bc3d92d72a9182e495474757
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-05-06 22:37:24 +0100

    Added testing of plugin component with injected services.

--- tests/ComponentPlugins8Test.php
+++ tests/ComponentPlugins8Test.php
@@ -69,4 +69,56 @@ class ComponentPlugins8Test extends DrupalCodeBuilderTestBase {
     $this->assertClass($plugin_file, 'Alpha', "The plugin class file contains contains the expected class.");
   }
 
+  /**
+   * Test Plugins component with injected services.
+   */
+  function testPluginsGenerationWithServices() {
+    $permission_name = 'my permission name';
+
+    // Create a module.
+    $module_name = 'test_module';
+    $module_data = array(
+      'base' => 'module',
+      'root_name' => $module_name,
+      'readable_name' => 'Test module',
+      'short_description' => 'Test Module description',
+      'hooks' => array(
+      ),
+      'plugins' => array(
+        0 => [
+          'plugin_type' => 'block',
+          'plugin_name' => 'alpha',
+          'injected_services' => [
+            'current_user',
+          ],
+        ],
+      ),
+      'requested_components' => array(
+      ),
+      'readme' => FALSE,
+    );
+    $files = $this->generateModuleFiles($module_data);
+    $file_names = array_keys($files);
+
+    $this->assertCount(2, $files, "Expected number of files is returned.");
+    $this->assertContains("$module_name.info.yml", $file_names, "The files list has a .info.yml file.");
+    $this->assertContains("src/Plugin/Block/Alpha.php", $file_names, "The files list has a plugin file.");
+
+    // Check the plugin file.
+    $plugin_file = $files["src/Plugin/Block/Alpha.php"];
+    $this->assertNoTrailingWhitespace($plugin_file, "The plugin class file contains no trailing whitespace.");
+    $this->assertNamespace($plugin_file, ['Drupal', $module_name, 'Plugin', 'Block'], "The plugin class file contains contains the expected namespace.");
+
+    $expected_annotation_properties = [
+      'id' => 'test_module_alpha',
+      // A value of NULL here means we don't test the value, only the key.
+      'admin_label' => NULL,
+      'category' => NULL,
+    ];
+    $this->assertClassAnnotation($plugin_file, 'Block', $expected_annotation_properties, "The plugin class has the correct annotation.");
+
+    $this->assertMethod($plugin_file, '__construct', "The plugin class has a constructor method.");
+    $this->assertMethod($plugin_file, 'create', "The plugin class has a constructor method.");
+  }
+
 }
