10cbe611e891ba587403e2f2421d9baea27d7688
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-06-15 07:21:16 +0100

    Added support for any service to class injected services (rather than just those gathered from \Drupal).

--- Generator/InjectedService.php
+++ Generator/InjectedService.php
@@ -23,31 +23,79 @@ class InjectedService extends BaseGenerator {
    * {@inheritdoc}
    */
   public function buildComponentContents($children_contents) {
+    $service_id = $this->component_data['service_id'];
+    $id_pieces = preg_split('@[_.]@', $service_id);
+
+    // First see if the requested service is in our stored data.
+    // (TODO: store data on ALL services -- but need to figure out a way to
+    // not give the user all ~500 in the options list!)
     $task_handler_report_services = \DrupalCodeBuilder\Factory::getTask('ReportServiceData');
     $services_data = $task_handler_report_services->listServiceData();
-    $service_data = $services_data[$this->component_data['service_id']];
 
+    $service_info = [];
+    $service_info['id'] = $service_id;
+
+    if (isset($services_data[$service_id])) {
+      // Copy these explicitly for maintainability and readability.
+      $service_info['description']  = $services_data[$service_id]['description'];
+      $service_info['interface']    = $services_data[$service_id]['interface'];
+    }
+    else {
+      // We don't have stored data about this service. Derive it from the actual
+      // service instead.
+      // Get the interface for the service.
+      $service = \Drupal::service($service_id);
+      $reflection = new \ReflectionClass($service);
+      // This can get us more than one interface, if the declared interface has
+      // parents. We only want one, so we need to go hacking in the code itself.
+      $file = $reflection->getFileName();
+      $start_line = $reflection->getStartLine();
+      $spl = new \SplFileObject($file);
+      $spl->seek($start_line - 1);
+      $service_declaration_line = $spl->current();
+
+      // Extract the interface from the declaration. We expect a service class
+      // to only name one interface in its declaration!
+      $matches = [];
+      preg_match("@implements (\w+)@", $service_declaration_line, $matches);
+      $interface_short_name = $matches[1];
+
+      // Find the fully-qualified interface name in the array of interfaces
+      // obtained from the reflection.
+      $interfaces = $reflection->getInterfaces();
+      foreach (array_keys($interfaces) as $interface) {
+        if (substr($interface, -(strlen($interface_short_name) + 1)) == '\\' . $interface_short_name) {
+          // The reflection doesn't give the initial '\'.
+          $full_interface_name = '\\' . $interface;
+        }
+      }
+
+      $service_label = implode(' ', $id_pieces);
+      $service_info['description']  = "The $service_label service";
+      $service_info['interface']    = $full_interface_name;
+    }
+
+    // Derive further information.
     // TODO: use NameFormattingTrait methods here.
-    $id_pieces = preg_split('@[_.]@', $service_data['id']);
-    $service_data['variable_name'] = implode('_', $id_pieces);
+    $service_info['variable_name'] = implode('_', $id_pieces);
     $id_pieces_first = array_shift($id_pieces);
-    $service_data['property_name'] = implode('', array_merge([$id_pieces_first], array_map('ucfirst', $id_pieces)));
+    $service_info['property_name'] = implode('', array_merge([$id_pieces_first], array_map('ucfirst', $id_pieces)));
 
     return [
       'service' => [
         'role' => 'service',
-        'content' => $service_data,
+        'content' => $service_info,
       ],
       'container_extraction' => [
         'role' => 'container_extraction',
-        'content' => "\$container->get('{$service_data['id']}'),",
+        'content' => "\$container->get('{$service_info['id']}'),",
       ],
       'constructor_param' => [
         'role' => 'constructor_param',
         'content' => [
-          'name'        => $service_data['variable_name'],
-          'typehint'    => $service_data['interface'],
-          'description' => $service_data['description'],
+          'name'        => $service_info['variable_name'],
+          'typehint'    => $service_info['interface'],
+          'description' => $service_info['description'],
         ],
       ],
     ];
