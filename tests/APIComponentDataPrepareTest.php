0557a7cccc8e753f622acf6a4afbc6372b1d4922
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-06-16 14:27:21 +0100

    Changed variable name.

--- tests/APIComponentDataPrepareTest.php
+++ tests/APIComponentDataPrepareTest.php
@@ -103,33 +103,33 @@ class APIComponentDataPrepareTest extends DrupalCodeBuilderTestBase {
 
     // The $component_type parameter passed in won't affect the call to
     // prepareComponentDataProperty(), so it's fine to say 'module'.
-    $mb_task_handler_generate = \DrupalCodeBuilder\Factory::getTask('Generate', 'module');
+    $generate = \DrupalCodeBuilder\Factory::getTask('Generate', 'module');
 
     $component_data = [];
 
     // Component data gets default values set if empty.
     // Simple properties.
     // Properties with no default still get something set into the data array.
-    $mb_task_handler_generate->prepareComponentDataProperty('no_default_string', $component_data_info['no_default_string'], $component_data);
+    $generate->prepareComponentDataProperty('no_default_string', $component_data_info['no_default_string'], $component_data);
     $this->assertArrayHasKey('no_default_string', $component_data,
       "The empty string default value was set in the component data.");
     $this->assertEquals($component_data['no_default_string'], '',
       "The fixed default value was set in the component data.");
 
-    $mb_task_handler_generate->prepareComponentDataProperty('no_default_array', $component_data_info['no_default_array'], $component_data);
+    $generate->prepareComponentDataProperty('no_default_array', $component_data_info['no_default_array'], $component_data);
     $this->assertArrayHasKey('no_default_array', $component_data,
       "The empty array default value was set in the component data.");
     $this->assertEquals($component_data['no_default_array'], [],
       "The fixed default value was set in the component data.");
 
     // Properties with an actual default value.
-    $mb_task_handler_generate->prepareComponentDataProperty('has_default_fixed', $component_data_info['has_default_fixed'], $component_data);
+    $generate->prepareComponentDataProperty('has_default_fixed', $component_data_info['has_default_fixed'], $component_data);
     $this->assertArrayHasKey('has_default_fixed', $component_data,
       "The fixed default value was set in the component data.");
     $this->assertEquals($component_data['has_default_fixed'], 'has_default_default',
       "The fixed default value was set in the component data.");
 
-    $mb_task_handler_generate->prepareComponentDataProperty('has_default_callable', $component_data_info['has_default_callable'], $component_data);
+    $generate->prepareComponentDataProperty('has_default_callable', $component_data_info['has_default_callable'], $component_data);
     $this->assertArrayHasKey('has_default_callable', $component_data,
       "The default value from a callable was set in the component data.");
     $this->assertEquals($component_data['has_default_callable'], 'has_default_default_called',
@@ -140,14 +140,14 @@ class APIComponentDataPrepareTest extends DrupalCodeBuilderTestBase {
 
     // Component data info options get filled out.
     // Simple properties.
-    $mb_task_handler_generate->prepareComponentDataProperty('has_options', $component_data_info['has_options'], $component_data);
+    $generate->prepareComponentDataProperty('has_options', $component_data_info['has_options'], $component_data);
     $this->assertArrayHasKey('A', $component_data_info['has_options']['options'],
       "The options were set in the component data info.");
     $this->assertArrayHasKey('B', $component_data_info['has_options']['options'],
       "The options were set in the component data info.");
 
     // Compound properties.
-    $mb_task_handler_generate->prepareComponentDataProperty('compound', $component_data_info['compound'], $component_data);
+    $generate->prepareComponentDataProperty('compound', $component_data_info['compound'], $component_data);
     $this->assertArrayHasKey('A', $component_data_info['compound']['properties']['compound_has_options']['options'],
       "The options were set in the component data info.");
     $this->assertArrayHasKey('B', $component_data_info['compound']['properties']['compound_has_options']['options'],
