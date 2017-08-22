7f35bcc15951ae8d7489ec6baee4413b91983866
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-08-22 21:39:17 +0100

    Added test for D7 menu items.

---
+++ Test/ComponentRouterItem7Test.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @file
+ * Contains ComponentRouterItem8Test.
+ */
+
+namespace DrupalCodeBuilder\Test;
+
+/**
+ * Tests for Router item component.
+ *
+ * Run with:
+ * @code
+ *   vendor/phpunit/phpunit/phpunit Test/ComponentRouterItem7Test.php
+ * @endcode
+ */
+class ComponentRouterItem7Test extends TestBase {
+
+  protected function setUp() {
+    $this->setupDrupalCodeBuilder(7);
+  }
+
+  /**
+   * Test generating a module with menu items.
+   */
+  public function testRouteGeneration() {
+    // Assemble module data.
+    $module_name = 'test_module';
+    $module_data = array(
+      'base' => 'module',
+      'root_name' => $module_name,
+      'readable_name' => 'Test Module',
+      'short_description' => 'Test Module description',
+      'router_items' => array(
+        0 => 'my/path',
+        1 => 'my/other-path',
+      ),
+      'readme' => FALSE,
+    );
+
+    $files = $this->generateModuleFiles($module_data);
+
+    $this->assertCount(2, $files, "The expected number of files is returned.");
+
+    $this->assertArrayHasKey("$module_name.info", $files, "The files list has a .info file.");
+    $this->assertArrayHasKey("$module_name.module", $files, "The files list has a module file.");
+
+    $module_file = $files["$module_name.module"];
+
+    $this->assertHookImplementation($module_file, 'hook_menu', $module_name);
+    $this->assertHookDocblock('hook_menu', $module_file, "The module file contains a hook_menu() implementation");
+
+    $this->assertFunctionCode($module_file, 'test_module_menu', "return \$items;");
+    $this->assertFunctionCode($module_file, 'test_module_menu', "\$items['my/path']");
+    $this->assertFunctionCode($module_file, 'test_module_menu', "\$items['my/other-path']");
+  }
+
+}
