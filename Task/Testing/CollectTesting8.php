85fc5fc5ab35dc2c1bb682f189cc1121b5b71167
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-08-23 14:45:33 +0100

    Fixed api file in test collection.

--- Task/Testing/CollectTesting8.php
+++ Task/Testing/CollectTesting8.php
@@ -38,7 +38,7 @@ class CollectTesting8 extends Collect8 {
       // Need this for hook_help().
       'help.api.php' => TRUE,
       // Need this for ThemeHook component.
-      'theme.api.php' => TRUE,
+      'core:theme.api.php' => TRUE,
     );
 
     $files = array_intersect_key($files, $testing_files);
