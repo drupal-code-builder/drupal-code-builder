5630960b01f1de581f78504787a1cdd15ad428ad
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-09-15 21:01:13 +0100

    Added helper for PHP qualified class names.

--- Generator/NameFormattingTrait.php
+++ Generator/NameFormattingTrait.php
@@ -30,4 +30,19 @@ trait NameFormattingTrait {
     return $camel;
   }
 
+  /**
+   * Helper to make a fully-qualified class name.
+   *
+   * @param array $class_name_pieces
+   *  An array of the class name pieces.
+   *
+   * @return
+   *  The qualified class name string, without the initial slash, e.g.
+   *  'Drupal\Foo\SomeClass'.
+   */
+  function makeQualifiedClassName($class_name_pieces) {
+    $qualified_class_name = implode('\\', $class_name_pieces);
+    return $qualified_class_name;
+  }
+
 }
