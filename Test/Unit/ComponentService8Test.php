86bdec47ec03c966a21ac924e0a24036c6ae2e90
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-09-15 23:05:38 +0100

    Added testing of extra parameters to service generator.

--- Test/Unit/ComponentService8Test.php
+++ Test/Unit/ComponentService8Test.php
@@ -64,4 +64,45 @@ class ComponentService8Test extends TestBase {
     $this->assertClass('MyService', $service_class_file, "The service file contains the service class.");
   }
 
+  /**
+   * Test generating a module with a service, specifying extra parameters.
+   */
+  public function testServiceGenerationWithParameters() {
+    // Assemble module data.
+    $module_name = 'test_module';
+    $module_data = array(
+      'base' => 'module',
+      'root_name' => $module_name,
+      'readable_name' => 'Test Module',
+      'short_description' => 'Test Module description',
+      'services' => array(
+        0 => [
+          'service_name' => 'my_service',
+          'injected_services' => [],
+          // Non-declared properties (for now!) that requesters can specify.
+          'prefixed_service_name' => 'my_prefix.my_service',
+          'plain_class_name' => 'MyServiceClass',
+        ],
+      ),
+      'readme' => FALSE,
+    );
+
+    $files = $this->generateModuleFiles($module_data);
+
+    $this->assertCount(3, $files, "Three files are returned.");
+
+    $this->assertArrayHasKey("src/MyServiceClass.php", $files, "The service class file has the specified name.");
+
+    $services_file = $files["$module_name.services.yml"];
+
+    $this->assertYamlProperty($services_file, 'services', NULL, "The services file has the services property.");
+    $this->assertYamlProperty($services_file, "my_prefix.my_service", NULL, "The services file declares the specified service name.");
+    $this->assertYamlProperty($services_file, 'class', "Drupal\\$module_name\\MyServiceClass", "The services file declares the service class.");
+
+    $service_class_file = $files["src/MyServiceClass.php"];
+
+    $this->assertNamespace(['Drupal', $module_name], $service_class_file, "The service class file contains contains the expected namespace.");
+    $this->assertClass('MyServiceClass', $service_class_file, "The service file contains the service class.");
+  }
+
 }
