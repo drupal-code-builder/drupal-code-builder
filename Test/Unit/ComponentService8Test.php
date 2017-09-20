0c8656467c72532748cd7e90ba8574db607117ac
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-09-20 07:39:23 +0100

    Added testing of injected services in service generator.

--- Test/Unit/ComponentService8Test.php
+++ Test/Unit/ComponentService8Test.php
@@ -65,6 +65,56 @@ class ComponentService8Test extends TestBase {
   }
 
   /**
+   * Test a service with with injected services.
+   */
+  function testServiceGenerationWithServices() {
+    // Assemble module data.
+    $module_name = 'test_module';
+    $module_data = array(
+      'base' => 'module',
+      'root_name' => $module_name,
+      'readable_name' => 'Test Module',
+      'short_description' => 'Test Module description',
+      'services' => array(
+        0 => [
+          'service_name' => 'my_service',
+          'injected_services' => [
+            'current_user',
+          ],
+        ],
+      ),
+      'readme' => FALSE,
+    );
+
+    $files = $this->generateModuleFiles($module_data);
+
+    $this->assertCount(3, $files, "Three files are returned.");
+
+    $this->assertArrayHasKey("$module_name.info.yml", $files, "The files list has a .info file.");
+    $this->assertArrayHasKey("$module_name.services.yml", $files, "The files list has a services yml file.");
+    $this->assertArrayHasKey("src/MyService.php", $files, "The files list has a service class file.");
+
+    $services_file = $files["$module_name.services.yml"];
+
+    $this->assertYamlProperty($services_file, 'services', NULL, "The services file has the services property.");
+    $this->assertYamlProperty($services_file, "$module_name.my_service", NULL, "The services file declares the service name.");
+    $this->assertYamlProperty($services_file, 'class', "Drupal\\$module_name\\MyService", "The services file declares the service class.");
+    // TODO: check service argument is present.
+
+    $service_class_file = $files["src/MyService.php"];
+
+    // Check the injected service.
+    $this->assertClassProperty('currentUser', $service_class_file, "The service class has a property for the injected service.");
+
+    $this->assertMethod('__construct', $service_class_file, "The service class has a constructor method.");
+    $parameters = [
+      'current_user',
+    ];
+    $this->assertFunctionHasParameters('__construct', $parameters, $service_class_file);
+    $this->assertFunctionCode($service_class_file, '__construct', '$this->currentUser = $current_user;');
+  }
+
+  /**
    * Test generating a module with a service, specifying extra parameters.
    */
   public function testServiceGenerationWithParameters() {
