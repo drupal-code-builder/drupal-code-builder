eb6270f35588a4834c9b958244e0e81fe406d3c1
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-10-02 21:05:38 +0100

    Refactored component collector helper to use a recursive method; moved handling of component properties into the system.

--- Task/Generate/ComponentCollector.php
+++ Task/Generate/ComponentCollector.php
@@ -3,13 +3,76 @@
 namespace DrupalCodeBuilder\Task\Generate;
 
 use DrupalCodeBuilder\Environment\EnvironmentInterface;
+use DrupalCodeBuilder\Generator\RootComponent;
 
 /**
  * Task helper for collecting components recursively.
+ *
+ * This takes a structured array of data for the root component, and produces
+ * an array of components, that is, instantiated Generator objects.
+ *
+ * Component data arrays have their structure defined by data property info
+ * arrays, which each Generator class defines in its componentDataDefinition()
+ * static method.
+ *
+ * The component data array for the root component, and any subsequent component
+ * data arrays, can produce generators in three ways:
+ * - The actual generator class for the data array in question; in other words,
+ *   a data array for a Module or a Permission will try to get a generator of
+ *   that type. ('Try' because of duplicates and merging: see below.)
+ * - Properties in the structured component data can be declared in the property
+ *   info as having a component themselves, with the 'component' key. This will
+ *   cause the value of this single property to be expanded and, upon suitable
+ *   treatment, passed back in as a component data array in its own right. The
+ *   nature of the expansion and treatment depends on the format of the
+ *   property:
+ *   - boolean: This simply represents whether the subcomponent exists or not.
+ *   - array: Each value in the array produces a component.
+ *   - compound: The data is an array keyed by delta, where each value is itself
+ *     a component data array.
+ * - Once a component is instantiated, its requiredComponents() method is
+ *   called, which returns an array where each value is a component data array.
+ *   Here, the component type is given in the data itself, in the
+ *   'component_type' property.
+ *
+ * It is possible for the attempted creation of a component to not produce a
+ * new component, if a component with the same unique ID has already been
+ * created. There are two cases where this happens:
+ * - The request data array for the new component and the existing one are
+ *   identical. Nothing further happens and the new component is discarded.
+ * - The request data arrays are different. The new request data array is merged
+ *   into the existing component.
  */
 class ComponentCollector {
 
+  /**
+   * Whether debug mode is enabled.
+   *
+   * TODO: Make this use an environment settings so UIs can pass it in.
+   *
+   * @var boolean
+   */
+  const DEBUG = FALSE;
+
+  /**
+   * The components collected by the process.
+   */
   protected $component_list = [];
+
+  /**
+   * The root component generator.
+   *
+   * @var \DrupalCodeBuilder\Generator\RootComponent
+   */
+  protected $root_component = NULL;
+
+  /**
+   * The record of requested data, keyed by generator ID.
+   *
+   * This allows us to prevent creation of duplicate generators which may be
+   * requested by different things.
+   */
+  protected $requested_data_record = [];
 
   /**
    * Constructs a new ComponentCollector.
@@ -51,7 +114,15 @@ class ComponentCollector {
    *  The list of components.
    */
   public function assembleComponentList($component_data) {
+    // Reset all class properties. We don't normally run this twice, but
+    // probably needed for tests.
+    $this->root_component = NULL;
+    $this->requested_data_record = [];
+    $this->component_list = [];
+
+    // Fiddly different handling for the type in the root component...
     $component_type = $component_data['base'];
+    $component_data['component_type'] = $component_type;
 
     // All generators assume that the root component has this property and
     // that it's required.
@@ -59,112 +130,168 @@ class ComponentCollector {
     // are passed along to requested components?
     // Or remove this when we are able to specify containment in requests
     // without needing to repeat the root name all the time?
-    $root_component_name = $component_data['root_name'];
+    $this->root_component_name = $component_data['root_name'];
 
-    // Process the root component's data.
+    $this->getComponentsFromData($component_type, $component_data);
+
+    return $this->component_list;
+  }
+
+  /**
+   * Create components from a data array.
+   *
+   * Provided this data does not duplicate already created components, the
+   * populates the $this->component_list property with:
+   * - The component itself given by the component_type property.
+   *
+   * @param $name
+   *   The name of the component in a containing array.
+   * @param $component_data
+   *   The data array. This must contain at least a 'component_type' property
+   *   the gives the type of the component.
+   */
+  protected function getComponentsFromData($name, $component_data) {
+    // Debugging: record the chain of how we get here each time.
+    static $chain;
+    $chain[] = $name;
+    $this->debug($chain, "collecting {$component_data['component_type']} $name", '-');
+
+    if (empty($component_data['component_type'])) {
+      throw new \Exception("Data for $name missing the 'component_type' property");
+    }
+
+    // Add the root component name to the data, except for the root component
+    // itself.
+    if (isset($this->root_component)) {
+      $component_data['root_component_name'] = $this->root_component_name;
+    }
+
+    // Process the component's data.
+    //dump($component_data);
+    $component_type = $component_data['component_type'];
     $this->processComponentData($component_data, $component_type);
 
-    // Get the root component generator.
-    // The component name is just the same as the type for the base generator.
-    $component_name = $component_type;
-    $root_component = $this->classHandler->getGenerator($component_type, $component_name, $component_data, NULL);
+    // Instantiate the generator in question.
+    // We always pass in the root component.
+    // We need to ensure that we create the root generator first, before we
+    // recurse, as all subsequent generators need it.
+    $generator = $this->classHandler->getGenerator($component_type, $name, $component_data, $this->root_component);
 
-    // Keep track of all requests to prevent duplicates.
-    $requested_info_record = array();
+    // If we've not yet set the root component, then this is the first
+    // generator we've created, and thus is the root component. Set it on the
+    // helper so we pass it to all subsequent generators.
+    // NOTE: For this to work properly, we have to instantiate the generator
+    // for this call before we recurse into properties or requirements!
+    if (is_null($this->root_component)) {
+      $this->root_component = $generator;
+    }
 
-    // The complete list we'll assemble. Start with the root component.
-    $component_list = array(
-      $root_component->getUniqueID() => $root_component,
-    );
+    $component_unique_id = $generator->getUniqueID();
+    $this->debug($chain, "instantiated name $name; type: $component_type; ID: $component_unique_id");
 
-    // Prep the current level with the root component for the first iteration.
-    $current_level = array(
-      $root_component->getUniqueID() => $root_component,
-    );
+    // Prevent re-requesting an identical previous request.
+    // TODO: use requestedComponentHandling() here?
+    if (isset($this->requested_data_record[$component_unique_id]) && $this->requested_data_record[$component_unique_id] == $component_data) {
+      $this->debug($chain, "bailing on name $name; type: $component_type; ID: $component_unique_id");
+      array_pop($chain);
 
-    $level_index = 0;
+      return;
+    }
+    $this->requested_data_record[$component_unique_id] = $component_data;
 
-    // Do a breadth-first tree traversal, working over the current level to
-    // create the next level, until there are no further items.
-    do {
-      $next_level = array();
+    //dump($this->requested_data_record);
 
-      // Log the current level.
-      $this->environment->log(array_keys($current_level), "starting level $level_index");
+    // A requested subcomponent may already exist in our tree.
+    if (isset($this->component_list[$component_unique_id])) {
+      // If it already exists, we merge the received data in with the
+      // existing component, and use the existing generator instead.
+      $generator = $this->component_list[$component_unique_id];
+      //dump("merging $component_unique_id");
+      $generator->mergeComponentData($component_data);
 
-      // Work over the current level, assembling a temporary array for the next
-      // level.
-      foreach ($current_level as $current_level_component_name => $item) {
-        // Each item of the current level gives us some children.
-        $item_required_subcomponent_list = $item->requiredComponents();
+      // We get required components from this even though we've already seen
+      // it, as it may need further components based on the data its just
+      // been given.
+    }
+    else {
+      // Add the new component to the complete array of components.
+      $this->component_list[$component_unique_id] = $generator;
+    }
 
-        // Instantiate each one (if not already done), and add it to the next
-        // level.
-        foreach ($item_required_subcomponent_list as $request_name => $data) {
-          // The $data may either be a string giving a class name, or an array.
-          if (is_string($data)) {
-            $component_type = $data;
-            $component_data = array();
+    // Pick out any data properties which are components themselves, and create the
+    // child components.
+    $component_data_info = $this->dataInfoGatherer->getComponentDataInfo($component_type, TRUE);
 
-            // Set the type in the array for consistency in debugging.
-            $component_data['component_type'] = $component_type;
+    foreach ($component_data_info as $property_name => $property_info) {
+      // We're only interested in component properties.
+      if (!isset($property_info['component'])) {
+        continue;
+      }
+      // Only work with properties for which there is data.
+      if (empty($component_data[$property_name])) {
+        continue;
+      }
+
+      // Get the component type.
+      $item_component_type = $property_info['component'];
+
+      switch ($property_info['format']) {
+        case 'compound':
+          // Create a component for each delta item.
+          foreach ($component_data[$property_name] as $delta => $item_data) {
+            $item_data['component_type'] = $item_component_type;
+
+            // Form an item request name with the delta.
+            $item_request_name = "{$item_component_type}_{$delta}";
+
+            // Recurse to create the component (and any child components, and
+            // any requests).
+            $this->getComponentsFromData($item_request_name, $item_data);
           }
-          else {
-            $component_type = $data['component_type'];
-            $component_data = $data;
+          break;
+        case 'boolean':
+          // We use the type as the request name; would it make more sense to
+          // use the property name?
+          $item_request_name = $item_component_type;
+          $item_data = [
+            'component_type' => $item_component_type,
+          ];
+          $this->getComponentsFromData($item_request_name, $item_data);
+          break;
+        case 'array':
+          foreach ($component_data[$property_name] as $item_value) {
+            $item_data = [
+              'component_type' => $item_component_type,
+            ];
+            // Each value in the array is the name of the component.
+            $item_request_name = $item_value;
+
+            $this->getComponentsFromData($item_request_name, $item_data);
           }
+          break;
+      }
+    }
 
-          // Add the root component name to the data.
-          $component_data['root_component_name'] = $root_component_name;
+    // Ask the generator for its required components.
+    $item_required_subcomponent_list = $generator->requiredComponents();
 
-          // Instantiate the component so we can get its unique ID.
-          // This may turn out to not be needed and get thrown away!
-          // Fill in defaults for the component data.
-          // (This is the equivalent of handling compound properties when
-          // processing the original input data.)
+    // Each item in the list is itself a component data array. Recurse for each
+    // one to get generators.
+    foreach ($item_required_subcomponent_list as $required_item_name => $required_item_data) {
+      // The data may either be a string giving a class name, or an array.
+      if (is_string($required_item_data)) {
+        $component_type = $required_item_data;
 
-          $this->processComponentData($component_data, $component_type);
+        $required_item_data = [
+          'component_type' => $required_item_data,
+        ];
+      }
 
-          // Instantiate the generator.
-          $generator = $this->classHandler->getGenerator($component_type, $request_name, $component_data, $root_component);
+      $this->getComponentsFromData($required_item_name, $required_item_data);
+    }
 
-          $component_unique_id = $generator->getUniqueID();
-
-          // Prevent re-requesting an identical previous request.
-          // TODO: use requestedComponentHandling() here?
-          if (isset($requested_info_record[$component_unique_id]) && $requested_info_record[$component_unique_id] == $data) {
-            continue;
-          }
-          $requested_info_record[$component_unique_id] = $data;
-
-          // A requested subcomponent may already exist in our tree.
-          if (isset($component_list[$component_unique_id])) {
-            // If it already exists, we merge the received data in with the
-            // existing component, and use the existing generator instead.
-            $generator = $component_list[$component_unique_id];
-            $generator->mergeComponentData($component_data);
-          }
-          else {
-            // Add the new component to the complete array of components.
-            $component_list[$component_unique_id] = $generator;
-          }
-
-          // Add the new component to the next level, whether it's new to us or
-          // not: if it's a repeat, we still need to ask it again for requests
-          // based on the new data it's just been given.
-          $next_level[$component_unique_id] = $generator;
-        } // each requested subcomponent from a component in the current level.
-      } // each component in the current level
-
-      // Now have all the next level.
-
-      // Set the next level to be current for the next loop.
-      $current_level = $next_level;
-      $level_index++;
-
-    } while (!empty($next_level));
-
-    return $component_list;
+    $this->debug($chain, "done");
+    array_pop($chain);
   }
 
   /**
@@ -174,7 +301,6 @@ class ComponentCollector {
    *  - sets default values on empty properties. To prevent a default being set
    *    and keep the component a property represents absent, set it to FALSE.
    *  - performs additional processing that a property may require
-   *  - expand properties that represent child components.
    *
    * @param &$component_data
    *  The component data array.
@@ -192,19 +318,12 @@ class ComponentCollector {
       // No need to set defaults on components here; the defaults will be filled
       // in when the component is instantiated in assembleComponentList() and
       // and this is called with the component's own data.
+      // TODO? Still true?
       if (isset($property_info['component'])) {
         continue;
       }
 
       $this->setComponentDataPropertyDefault($property_name, $property_info, $component_data, 'process');
-
-      if (isset($property_info['properties']) && isset($component_data[$property_name]) && is_array($component_data[$property_name])) {
-        foreach ($component_data[$property_name] as $delta => $delta_data) {
-          foreach ($property_info['properties'] as $child_property_name => $child_property_info) {
-            $this->setComponentDataPropertyDefault($child_property_name, $child_property_info, $component_data[$property_name][$delta], 'process');
-          }
-        }
-      }
     }
 
     // Allow each property to apply its processing callback. Note that this may
@@ -216,44 +335,6 @@ class ComponentCollector {
         $processing_callback($component_data[$property_name], $component_data, $property_info);
       }
     } // processing callback
-
-    // Expand any properties that represent child components to add.
-    // TODO: This is a fairly rough piece of functionality that needs more
-    // thought.
-    foreach ($component_data_info as $property_name => $property_info) {
-      if (isset($property_info['component']) && !empty($component_data[$property_name])) {
-        // Get the component type.
-        $component_type = $property_info['component'];
-
-        // If the format is 'compound', handling type is irrelevant.
-        if ($property_info['format'] == 'compound') {
-          foreach ($component_data[$property_name] as $delta => $item_data) {
-            $item_data['component_type'] = $component_type;
-
-            $component_data['requested_components']["{$component_type}_{$delta}"] = $item_data;
-          }
-
-          continue;
-        }
-
-        // Ask the component type class how to handle this.
-        $handling_type = $this->classHandler->getRepeatComponentHandling($component_type);
-
-        switch ($handling_type) {
-          case 'singleton':
-            // The component type can only occur once and therefore the name is
-            // the same as the type.
-            $component_data['requested_components'][$component_type] = $component_type;
-            break;
-          case 'repeat':
-            // Each value in the array is the name of a component.
-            foreach ($component_data[$property_name] as $requested_component_name) {
-              $component_data['requested_components'][$requested_component_name] = $component_type;
-            }
-            break;
-        }
-      }
-    } // expand components
   }
 
   /**
@@ -318,6 +399,22 @@ class ComponentCollector {
         $component_data_local[$property_name] = $default_value;
       }
     }
+  }
+
+  /**
+   * Output debug message, with indentation for the current iteration.
+   *
+   * @param string[] $chain
+   *   The current chain of component names.
+   * @param string $message
+   *   A message to output.
+   */
+  protected function debug($chain, $message, $indent_char = ' ') {
+    if (!self::DEBUG) {
+      return;
+    }
+
+    dump(str_repeat($indent_char, count($chain)) . ' ' . implode(':', $chain) . ': ' . $message);
   }
 
 }
