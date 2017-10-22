25b396f682e3ef1cbb81ff6050ecdadcc2080f0d
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-10-22 20:52:20 +0100

    Moved base fields property from parent class to content entity generator.

--- Generator/EntityTypeBase.php
+++ Generator/EntityTypeBase.php
@@ -79,51 +79,7 @@ abstract class EntityTypeBase extends PHPClassFile {
         // TODO: methods from this in the entity class!
         // TODO: use corresponding traits, eg EntityChangedTrait;
       ],
-      'base_fields' => [
-        'label' => 'Base fields',
-        'description' => "The machine name of the field.",
-        'format' => 'compound',
-        // TODO: default, populated by things such as interface choice!
-        'properties' => [
-          'name' => [
-            'label' => 'Field name',
-            'required' => TRUE,
-          ],
-          'label' => [
-            'label' => 'Field label',
-            'default' => function($component_data) {
-              $entity_type_id = $component_data['name'];
-              return self::snakeToTitle($entity_type_id);
-            },
-            'process_default' => TRUE,
-          ],
-          'type' => [
-            'label' => 'Field type',
-            'required' => TRUE,
-            'options' => [
-              // TODO: get these from Field/FieldType plugins, via collection,
-              // or from the environment on the fly.
-              // TODO: config entities don't have the same types.
-              'string' => 'A simple string.',
-              'boolean' => 'A boolean stored as an integer.',
-              'integer' => 'An integer, with settings for min and max value validation (also provided for decimal and float)',
-              'decimal' => 'A decimal with configurable precision and scale.',
-              'float' => 'A float number',
-              'language' => 'Contains a language code and the language as a computed property',
-              'timestamp' => 'A Unix timestamp stored as an integer',
-              'created' => 'A timestamp that uses the current time as a default value.',
-              'changed' => 'A timestamp that is automatically updated to the current time if the entity is saved.',
-              'datetime' => 'A date stored as an ISO 8601 string.',
-              'uri' => 'Contains a URI. The link module also provides a link field type that can include a link title and can point to an internal or external URI/route.',
-              'uuid' => 'A UUID field that generates a new UUID as the default value.',
-              'email' => 'An email, with corresponding validation and widgets and formatters.',
-              'entity_reference' => 'An entity reference with a target_id and a computed entity field property. entity_reference.module provides widgets and formatters when enabled.',
-              // Broken in core!
-              //'map' => 'Can contain any number of arbitrary properties, stored as a serialized string',
-            ],
-          ],
-        ],
-      ],
+      // TODO: schema properties for config entities.
       'entity_keys' => [
         'label' => 'Entity keys',
         'computed' => TRUE,
