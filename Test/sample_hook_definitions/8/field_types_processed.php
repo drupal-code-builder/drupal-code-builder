e8f9b859d1795bf8f3302c962126c6809d9cd70e
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-10-22 15:01:51 +0100

    Added field type data to testing sample data.

---
+++ Test/sample_hook_definitions/8/field_types_processed.php
@@ -0,0 +1,19 @@
+<?php $data =
+array (
+  'text' => 
+  array (
+    'type' => 'text',
+    'label' => 'Text (formatted)',
+    'description' => 'This field stores a text with a text format.',
+    'default_widget' => 'text_textfield',
+    'default_formatter' => 'text_default',
+  ),
+  'boolean' => 
+  array (
+    'type' => 'boolean',
+    'label' => 'Boolean',
+    'description' => 'An entity field containing a boolean value.',
+    'default_widget' => 'boolean_checkbox',
+    'default_formatter' => 'boolean',
+  ),
+);
