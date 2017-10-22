9cd93e0dac5c5abb229e42d4542a55a5780bdc47
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-10-22 21:11:12 +0100

    Added field type data options to content entity base fields.

--- Generator/ContentEntityType.php
+++ Generator/ContentEntityType.php
@@ -63,26 +63,7 @@ class ContentEntityType extends EntityTypeBase {
           'type' => [
             'label' => 'Field type',
             'required' => TRUE,
-            'options' => [
-              // TODO: get these from Field/FieldType plugins, via collection,
-              // or from the environment on the fly.
-              'string' => 'A simple string.',
-              'boolean' => 'A boolean stored as an integer.',
-              'integer' => 'An integer, with settings for min and max value validation (also provided for decimal and float)',
-              'decimal' => 'A decimal with configurable precision and scale.',
-              'float' => 'A float number',
-              'language' => 'Contains a language code and the language as a computed property',
-              'timestamp' => 'A Unix timestamp stored as an integer',
-              'created' => 'A timestamp that uses the current time as a default value.',
-              'changed' => 'A timestamp that is automatically updated to the current time if the entity is saved.',
-              'datetime' => 'A date stored as an ISO 8601 string.',
-              'uri' => 'Contains a URI. The link module also provides a link field type that can include a link title and can point to an internal or external URI/route.',
-              'uuid' => 'A UUID field that generates a new UUID as the default value.',
-              'email' => 'An email, with corresponding validation and widgets and formatters.',
-              'entity_reference' => 'An entity reference with a target_id and a computed entity field property. entity_reference.module provides widgets and formatters when enabled.',
-              // Broken in core!
-              //'map' => 'Can contain any number of arbitrary properties, stored as a serialized string',
-            ],
+            'options' => 'ReportFieldTypes:listFieldTypesOptions',
           ],
         ],
       ],
