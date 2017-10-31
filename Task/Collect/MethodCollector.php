da2aa0bea011d9c0fd2b58824417ee466b26dc7e
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-10-31 15:43:31 +0000

    Moved code for getting data on a single method to a separate method.

--- Task/Collect/MethodCollector.php
+++ Task/Collect/MethodCollector.php
@@ -14,6 +14,8 @@ class MethodCollector {
   /**
    * Get data for the methods of a class or interface.
    *
+   * TODO: remove this, use getMethodData() from other classes.
+   *
    * @param $name
    *  The fully-qualified name of the class or interface.
    *
@@ -36,61 +38,77 @@ class MethodCollector {
         //continue;
       }
 
-      $interface_method_data = [];
-
-      $interface_method_data['name'] = $method->getName();
-
-      // Methods may be in parent interfaces, so not all in the same file.
-      $filename = $method->getFileName();
-      $source = file($filename);
-      $start_line = $method->getStartLine();
-
-      // Trim whitespace from the front, as this will be indented.
-      $interface_method_data['declaration'] = trim($source[$start_line - 1]);
-
-      // Get the docblock for the method.
-      $method_docblock_lines = explode("\n", $method->getDocComment());
-      foreach ($method_docblock_lines as $line) {
-        // Take the first actual docblock line to be the description.
-        if (substr($line, 0, 5) == '   * ') {
-          $interface_method_data['description'] = substr($line, 5);
-          break;
-        }
-      }
-
-      // Replace class typehints on method parameters with their full namespaced
-      // versions, as typically these will be short class names. The PHPFile
-      // generator will then take care of extracting namespaces and creating
-      // import statements.
-      // Get the typehint classes on parameters.
-      $parameters = $method->getParameters();
-      $parameter_hinted_class_short_names = [];
-      $parameter_hinted_class_full_names = [];
-      foreach ($parameters as $parameter) {
-        $parameter_hinted_class = $parameter->getClass();
-
-        // Skip a parameter that doesn't have a class hint.
-        if (is_null($parameter_hinted_class)) {
-          continue;
-        }
-
-        // Create arrays for str_replace() of short and long classnames.
-        $parameter_hinted_class_short_names[] = $parameter_hinted_class->getShortName();
-        // The PHPFile generator works with fully-qualified classnames, with
-        // an initial '\', so we need to prepend that.
-        $parameter_hinted_class_full_names[] = '\\' . $parameter_hinted_class->getName();
-      }
-
-      $interface_method_data['declaration'] = str_replace(
-        $parameter_hinted_class_short_names,
-        $parameter_hinted_class_full_names,
-        $interface_method_data['declaration']
-      );
-
-      $data[$method->getName()] = $interface_method_data;
+      $data[$method->getName()] = $this->getMethodData($method);
     }
 
     return $data;
+  }
+
+  /**
+   * Gets data for a method.
+   *
+   * @param \ReflectionMethod $method
+   *  The reflection object for the method.
+   *
+   * @return
+   *  An array containing:
+   *  - 'name: The name of the method.
+   *  - 'declaration': The function declaration line.
+   *  - 'description': The description from the method's docblock first line.
+   */
+  public function getMethodData(\ReflectionMethod $method) {
+    $interface_method_data = [];
+
+    $interface_method_data['name'] = $method->getName();
+
+    // Methods may be in parent interfaces, so not all in the same file.
+    $filename = $method->getFileName();
+    $source = file($filename);
+    $start_line = $method->getStartLine();
+
+    // Trim whitespace from the front, as this will be indented.
+    $interface_method_data['declaration'] = trim($source[$start_line - 1]);
+
+    // Get the docblock for the method.
+    $method_docblock_lines = explode("\n", $method->getDocComment());
+    foreach ($method_docblock_lines as $line) {
+      // Take the first actual docblock line to be the description.
+      if (substr($line, 0, 5) == '   * ') {
+        $interface_method_data['description'] = substr($line, 5);
+        break;
+      }
+    }
+
+    // Replace class typehints on method parameters with their full namespaced
+    // versions, as typically these will be short class names. The PHPFile
+    // generator will then take care of extracting namespaces and creating
+    // import statements.
+    // Get the typehint classes on parameters.
+    $parameters = $method->getParameters();
+    $parameter_hinted_class_short_names = [];
+    $parameter_hinted_class_full_names = [];
+    foreach ($parameters as $parameter) {
+      $parameter_hinted_class = $parameter->getClass();
+
+      // Skip a parameter that doesn't have a class hint.
+      if (is_null($parameter_hinted_class)) {
+        continue;
+      }
+
+      // Create arrays for str_replace() of short and long classnames.
+      $parameter_hinted_class_short_names[] = $parameter_hinted_class->getShortName();
+      // The PHPFile generator works with fully-qualified classnames, with
+      // an initial '\', so we need to prepend that.
+      $parameter_hinted_class_full_names[] = '\\' . $parameter_hinted_class->getName();
+    }
+
+    $interface_method_data['declaration'] = str_replace(
+      $parameter_hinted_class_short_names,
+      $parameter_hinted_class_full_names,
+      $interface_method_data['declaration']
+    );
+
+    return $interface_method_data;
   }
 
 }
