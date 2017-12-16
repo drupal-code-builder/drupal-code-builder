673ed0a4576bbcaa3a65db2fb5c5c12d161bd564
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-12-16 13:31:53 +0000

    Added properties to info generator to acquire from the requesting module.

--- Generator/Info.php
+++ Generator/Info.php
@@ -15,6 +15,30 @@ class Info extends File {
   /**
    * {@inheritdoc}
    */
+  public static function componentDataDefinition() {
+    $data_definition = parent::componentDataDefinition();
+
+    // Properties acquired from the requesting PluginType component.
+    $plugin_type_properties = [
+      'readable_name',
+      // Only needed for D8 info files.
+      'base',
+      'short_description',
+      'module_dependencies',
+      'module_package',
+    ];
+    foreach ($plugin_type_properties as $property_name) {
+      $data_definition[$property_name] = [
+        'acquired' => TRUE,
+      ];
+    }
+
+    return $data_definition;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function buildComponentContents($children_contents) {
     $lines = array();
     foreach ($this->filterComponentContentsForRole($children_contents, 'infoline') as $component_name => $component_lines) {
