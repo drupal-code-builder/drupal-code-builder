1a5c23c40d9157958b036b5ac623d6e956599269
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-01-11 21:13:30 +0000

    Changed test to use generate helper rather than the Generate task.

--- Test/Unit/APIComponentDataPrepareTest.php
+++ Test/Unit/APIComponentDataPrepareTest.php
@@ -122,35 +122,34 @@ class APIComponentDataPrepareTest extends TestBase {
       ]
     ];
 
-    // The $component_type parameter passed in won't affect the call to
-    // prepareComponentDataProperty(), so it's fine to say 'module'.
-    $generate = \DrupalCodeBuilder\Factory::getTask('Generate', 'module');
+    // Create the helper.
+    $component_property_preparer = new \DrupalCodeBuilder\Task\Generate\ComponentPropertyPreparer();
 
     $component_data = [];
 
     // Component data gets default values set if empty.
     // Simple properties.
     // Properties with no default still get something set into the data array.
-    $generate->prepareComponentDataProperty('no_default_string', $component_data_info['no_default_string'], $component_data);
+    $component_property_preparer->prepareComponentDataProperty('no_default_string', $component_data_info['no_default_string'], $component_data);
     $this->assertArrayHasKey('no_default_string', $component_data,
       "The empty string default value was set in the component data.");
     $this->assertEquals($component_data['no_default_string'], '',
       "The empty string default value was set in the component data.");
 
-    $generate->prepareComponentDataProperty('no_default_array', $component_data_info['no_default_array'], $component_data);
+    $component_property_preparer->prepareComponentDataProperty('no_default_array', $component_data_info['no_default_array'], $component_data);
     $this->assertArrayHasKey('no_default_array', $component_data,
       "The empty array default value was set in the component data.");
     $this->assertEquals($component_data['no_default_array'], [],
       "The empty array default value was set in the component data.");
 
     // Properties with an actual default value.
-    $generate->prepareComponentDataProperty('has_default_fixed', $component_data_info['has_default_fixed'], $component_data);
+    $component_property_preparer->prepareComponentDataProperty('has_default_fixed', $component_data_info['has_default_fixed'], $component_data);
     $this->assertArrayHasKey('has_default_fixed', $component_data,
       "The fixed default value was set in the component data.");
     $this->assertEquals($component_data['has_default_fixed'], 'has_default_default',
       "The fixed default value was set in the component data.");
 
-    $generate->prepareComponentDataProperty('has_default_callable', $component_data_info['has_default_callable'], $component_data);
+    $component_property_preparer->prepareComponentDataProperty('has_default_callable', $component_data_info['has_default_callable'], $component_data);
     $this->assertArrayHasKey('has_default_callable', $component_data,
       "The default value from a callable was set in the component data.");
     $this->assertEquals($component_data['has_default_callable'], 'has_default_default_called',
@@ -160,7 +159,7 @@ class APIComponentDataPrepareTest extends TestBase {
     // Initialize a child item for the compound property: defaults will only be
     // set if this exists.
     $component_data['compound_defaults'][0] = [];
-    $generate->prepareComponentDataProperty(
+    $component_property_preparer->prepareComponentDataProperty(
       'compound_has_default_fixed',
       $component_data_info['compound_defaults']['properties']['compound_has_default_fixed'],
       $component_data['compound_defaults'][0]
@@ -170,7 +169,7 @@ class APIComponentDataPrepareTest extends TestBase {
     $this->assertEquals('compound_has_default_fixed_default_value', $component_data['compound_defaults'][0]['compound_has_default_fixed'],
       "The fixed default value was set in the component data child item.");
 
-    $generate->prepareComponentDataProperty(
+    $component_property_preparer->prepareComponentDataProperty(
       'compound_has_default_callable',
       $component_data_info['compound_defaults']['properties']['compound_has_default_callable'],
       $component_data['compound_defaults'][0]
@@ -182,7 +181,7 @@ class APIComponentDataPrepareTest extends TestBase {
 
     // Create a 2nd child to check this works.
     $component_data['compound_defaults'][1] = [];
-    $generate->prepareComponentDataProperty(
+    $component_property_preparer->prepareComponentDataProperty(
       'compound_has_default_fixed',
       $component_data_info['compound_defaults']['properties']['compound_has_default_fixed'],
       $component_data['compound_defaults'][1]
@@ -192,7 +191,7 @@ class APIComponentDataPrepareTest extends TestBase {
     $this->assertEquals('compound_has_default_fixed_default_value', $component_data['compound_defaults'][1]['compound_has_default_fixed'],
       "The fixed default value was set in the component data child item.");
 
-    $generate->prepareComponentDataProperty(
+    $component_property_preparer->prepareComponentDataProperty(
       'compound_has_default_callable',
       $component_data_info['compound_defaults']['properties']['compound_has_default_callable'],
       $component_data['compound_defaults'][1]
@@ -204,21 +203,21 @@ class APIComponentDataPrepareTest extends TestBase {
 
     // Component data info options get filled out.
     // Simple properties.
-    $generate->prepareComponentDataProperty('has_options', $component_data_info['has_options'], $component_data);
+    $component_property_preparer->prepareComponentDataProperty('has_options', $component_data_info['has_options'], $component_data);
     $this->assertArraySubset(['A' => 'option_a'], $component_data_info['has_options']['options'],
       "The options were set in the component data info.");
     $this->assertArraySubset(['B' => 'option_b'], $component_data_info['has_options']['options'],
       "The options were set in the component data info.");
 
     // Presets.
-    $generate->prepareComponentDataProperty('has_presets', $component_data_info['has_presets'], $component_data);
+    $component_property_preparer->prepareComponentDataProperty('has_presets', $component_data_info['has_presets'], $component_data);
     $this->assertArraySubset(['A' => 'option_a'], $component_data_info['has_presets']['options']);
     $this->assertArraySubset(['B' => 'option_b'], $component_data_info['has_presets']['options']);
 
     // Compound properties.
     // If we're interested in options, we can prepare the compound property in
     // one go.
-    $generate->prepareComponentDataProperty('compound_options', $component_data_info['compound_options'], $component_data);
+    $component_property_preparer->prepareComponentDataProperty('compound_options', $component_data_info['compound_options'], $component_data);
     $this->assertArraySubset(['A' => 'option_a'], $component_data_info['compound_options']['properties']['compound_has_options']['options'],
       "The options were set in the component data info.");
     $this->assertArraySubset(['B' => 'option_b'], $component_data_info['compound_options']['properties']['compound_has_options']['options'],
