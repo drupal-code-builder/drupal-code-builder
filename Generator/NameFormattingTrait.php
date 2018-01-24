2d3b671bb38d6031ebab71b443881e8b5501cfd6
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-01-24 17:27:10 +0000

    Added to docs.

--- Generator/NameFormattingTrait.php
+++ Generator/NameFormattingTrait.php
@@ -11,7 +11,8 @@ trait NameFormattingTrait {
    * Helper to make a fully-qualified class name.
    *
    * @param array $class_name_pieces
-   *  An array of the class name pieces.
+   *  An array of the class name pieces. It is permissible for some pieces to
+   *  contain more than one subnamespaces.
    *
    * @return
    *  The qualified class name string, without the initial slash, e.g.
