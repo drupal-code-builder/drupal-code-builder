630fefeaa7ed48987b99b9c0ab12d8b24869d50e
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-02-16 22:25:39 +0000

    Added test assertions for YAML properties inline or expanded.

--- Test/Unit/ParserYamlTest.php
+++ Test/Unit/ParserYamlTest.php
@@ -85,4 +85,104 @@ EOT;
     ];
   }
 
+  /**
+   * Tests the assertPropertyIsExpanded()/assertPropertyIsInlined() assertions.
+   *
+   * @dataProvider providerAssertPropertyExpandedInline
+   */
+  public function testAssertPropertyExpandedInline($property, $expanded) {
+    $yaml = <<<EOT
+services:
+  test_module.alpha:
+    class: Drupal\test_module\Alpha
+    arguments:
+      - '@current_user'
+      - '@entity_type.manager'
+    tags:
+      - { name: foo, priority: 0 }
+  test_module.beta:
+    class: Drupal\test_module\Alpha
+    arguments: ['@current_user']
+    tags:
+      - { name: bar, priority: 0 }
+EOT;
+
+    $yaml_tester = new YamlTester($yaml);
+
+    try {
+      $yaml_tester->assertPropertyIsExpanded($property);
+      // Assertion passed.
+      if (!$expanded) {
+        $this->fail("assertPropertyIsExpanded() should fail");
+      }
+    }
+    catch (ExpectationFailedException $e) {
+      // Assertion failed.
+      if ($expanded) {
+        $this->fail("assertPropertyIsExpanded() should pass.");
+      }
+    }
+
+    try {
+      $yaml_tester->assertPropertyIsInlined($property);
+      // Assertion passed.
+      if ($expanded) {
+        $this->fail("assertPropertyIsInlined() should fail");
+      }
+    }
+    catch (ExpectationFailedException $e) {
+      // Assertion failed.
+      if (!$expanded) {
+        $this->fail("assertPropertyIsInlined() should pass.");
+      }
+    }
+  }
+
+  /**
+   * Data provider for testAssertPropertyExpandedInline().
+   */
+  public function providerAssertPropertyExpandedInline() {
+    return [
+      'root' => [
+        ['services'],
+        TRUE,
+      ],
+      'alpha_service' => [
+        ['services', 'test_module.alpha'],
+        TRUE,
+      ],
+      'alpha_class' => [
+        ['services', 'test_module.alpha', 'class'],
+        TRUE,
+      ],
+      'alpha_args' => [
+        ['services', 'test_module.alpha', 'arguments'],
+        TRUE,
+      ],
+      'alpha_tags' => [
+        ['services', 'test_module.alpha', 'tags'],
+        TRUE,
+      ],
+      'alpha_tags_0' => [
+        ['services', 'test_module.alpha', 'tags', 0],
+        TRUE,
+      ],
+      'alpha_name' => [
+        ['services', 'test_module.alpha', 'tags', 0, 'name'],
+        FALSE,
+      ],
+      'beta_service' => [
+        ['services', 'test_module.beta'],
+        TRUE,
+      ],
+      'beta_args' => [
+        ['services', 'test_module.beta', 'arguments'],
+        TRUE,
+      ],
+      'beta_args_0' => [
+        ['services', 'test_module.beta', 'arguments', 0],
+        FALSE,
+      ],
+    ];
+  }
 }
--- Test/Unit/Parsing/YamlTester.php
+++ Test/Unit/Parsing/YamlTester.php
@@ -92,6 +92,145 @@ class YamlTester {
   }
 
   /**
+   * Asserts a property is formatted as expanded.
+   *
+   * @param string[] $property_address
+   *   The address of the property.
+   * @param string $message
+   *   (optional) The assertion message.
+   */
+  public function assertPropertyIsExpanded($property_address, $message = NULL) {
+    $message = $message ?? 'The YAML property is formated as expanded.';
+
+    $line_index = $this->findYamlLine($property_address);
+
+    Assert::assertNotSame($line_index, FALSE, $message);
+  }
+
+  /**
+   * Asserts a property is formatted as inline, with parent expanded.
+   *
+   * @param string[] $property_address
+   *   The address of the property.
+   * @param string $message
+   *   (optional) The assertion message.
+   */
+  public function assertPropertyIsInlined($property_address, $message = NULL) {
+    $message = $message ?? 'The YAML property is formated as inline.';
+
+    // Assert the property actually exists.
+    $this->assertHasProperty($property_address);
+
+    // If we can find a line for the property, it's expanded, not inline.
+    $line_index = $this->findYamlLine($property_address);
+    Assert::assertSame($line_index, FALSE, $message);
+
+    // We want the property one level up to be expanded, so check for that.
+    $property_parent_address = $property_address;
+    array_pop($property_parent_address);
+    $line_index = $this->findYamlLine($property_parent_address);
+    Assert::assertNotSame($line_index, FALSE, $message);
+  }
+
+  /**
+   * Finds the index for a property's line.
+   *
+   * @param string[] $property_address
+   *   The address of the property.
+   *
+   * @return int|false
+   *   The line number if the property is found on its own line. FALSE if the
+   *   property is not found.
+   */
+  public function findYamlLine($property_address) {
+    // A copy of the property address array to search with destructively.
+    $property_address_search = $property_address;
+
+    // Get an array of the original YAML lines, using string keys so that
+    // we can use array_slice() on it without losing the line numbers.
+    $yaml_string_lines = [];
+    foreach (explode("\n", $this->originalYaml) as $index => $line) {
+      $yaml_string_lines["line_{$index}"] = $line;
+    }
+
+    // Look for the each property in the property address.
+    // The current address level.
+    $level = 0;
+    $last_found_level = NULL;
+    // A copy of the YAML lines array to search in destructively.
+    $yaml_string_lines_slice_to_search = $yaml_string_lines;
+
+    while ($property_address_search) {
+      // (Can't array_shift() in the while() as a property may be a 0.)
+      $property = array_shift($property_address_search);
+
+      // The expected number of indented spaces for this level's property.
+      $indent_count = $level * 2;
+
+      if (is_numeric($property)) {
+        // Numeric property.
+        // We know how many children in we need to be.
+        // Check the prior children, if any.
+        $previous_lines = array_slice($yaml_string_lines_slice_to_search, 0, $property);
+
+        // All the previous lines should have the right indent.
+        foreach ($previous_lines as $line) {
+          if (!preg_match("@^ {{$indent_count}}- @", $line)) {
+            // A previous line is not a child of the previous level property:
+            // it does not have enough child numeric properties.
+            return FALSE;
+          }
+        }
+
+        $expected_line_array = array_slice($yaml_string_lines_slice_to_search, $property, 1);
+        $expected_line = reset($expected_line_array);
+        if (!preg_match("@^ {{$indent_count}}- @", $expected_line)) {
+          // The expected line is not a child numeric property.
+          return FALSE;
+        }
+
+        $last_found_level = $level;
+        $found_line_key = key($expected_line_array);
+        $found_line_index = substr($line_key, 5);
+      }
+      else {
+        // String property.
+        foreach ($yaml_string_lines_slice_to_search as $line_key => $line) {
+          if (!preg_match("@^ {{$indent_count}}@", $line)) {
+            // We've reached a line that is not a child of the previous property:
+            // we've failed to find the current one as a child of it.
+
+            break;
+          }
+
+          if (preg_match("@^ {{$indent_count}}{$property}:@", $line)) {
+            $last_found_level = $level;
+            $found_line_key = $line_key;
+            $found_line_index = substr($line_key, 5);
+
+            break;
+          }
+        } // foreach YAML
+      }
+
+      // The break statements come here.
+
+      if ($last_found_level != $level) {
+        // We didn't find anything for this level: fail.
+        return FALSE;
+      }
+
+      // Trim the YAML lines to search in, so we start at the line after
+      // the one we've just found.
+      $yaml_string_lines_slice_to_search = array_slice($yaml_string_lines, $found_line_index + 1);
+
+      $level++;
+    } // foreach property
+
+    return $found_line_index;
+  }
+
+  /**
    * Copy of Drupal's NestedArray::keyExists().
    */
   protected function keyExists(array $array, array $parents) {
