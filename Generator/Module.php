a5cbf44c1f2c59335a8f2f7b673bff70659055a1
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-02-25 07:25:12 +0000

    Fixed '%Module' token doing ucfirst rather than using readable name property.

--- Generator/Module.php
+++ Generator/Module.php
@@ -438,7 +438,7 @@ class Module extends RootComponent {
 
     return array(
       '%module'       => $module_data['root_name'],
-      '%Module'       => ucfirst($module_data['root_name']),
+      '%Module'       => ucfirst($module_data['readable_name']),
       '%description'  => str_replace("'", "\'", $module_data['short_description']),
       '%name'         => !empty($module_data['readable_name']) ? str_replace("'", "\'", $module_data['readable_name']) : $module_data['root_name'],
       '%help'         => !empty($module_data['module_help_text']) ? str_replace('"', '\"', $module_data['module_help_text']) : 'TODO: Create admin help text.',
