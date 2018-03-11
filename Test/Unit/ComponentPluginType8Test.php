2ba14bc8f665c82c316735129105421274730e70
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-03-11 13:34:17 +0000

    Changed plugin type test to use PHP helper.

--- Test/Unit/ComponentPluginType8Test.php
+++ Test/Unit/ComponentPluginType8Test.php
@@ -2,6 +2,7 @@
 
 namespace DrupalCodeBuilder\Test\Unit;
 
+use DrupalCodeBuilder\Test\Unit\Parsing\PHPTester;
 use DrupalCodeBuilder\Test\Unit\Parsing\YamlTester;
 
 /**
@@ -9,7 +10,7 @@ use DrupalCodeBuilder\Test\Unit\Parsing\YamlTester;
  *
  * @group yaml
  */
-class ComponentPluginType8Test extends TestBaseComponentGeneration {
+class ComponentPluginType8Test extends TestBase {
 
   /**
    * The Drupal core major version to set up for this test.
@@ -49,22 +50,22 @@ class ComponentPluginType8Test extends TestBaseComponentGeneration {
 
     // Check the plugin manager file.
     $plugin_manager_file = $files["src/CatFeederManager.php"];
-    $this->assertWellFormedPHP($plugin_manager_file);
-    $this->assertDrupalCodingStandards($plugin_manager_file);
-    $this->parseCode($plugin_manager_file);
-    $this->assertHasClass('Drupal\test_module\CatFeederManager');
-    $this->assertClassHasParent('Drupal\Core\Plugin\DefaultPluginManager');
+
+    $php_tester = new PHPTester($plugin_manager_file);
+    $php_tester->assertDrupalCodingStandards();
+    $php_tester->assertHasClass('Drupal\test_module\CatFeederManager');
+    $php_tester->assertClassHasParent('Drupal\Core\Plugin\DefaultPluginManager');
 
     // Check the __construct() method's parameters.
-    $this->assertMethodHasParameters([
+    $php_tester->assertMethodHasParameters([
       'namespaces' => 'Traversable',
       'cache_backend' => 'Drupal\Core\Cache\CacheBackendInterface',
       'module_handler' => 'Drupal\Core\Extension\ModuleHandlerInterface',
     ], '__construct');
 
     // Check the __construct() method's statements.
-    $this->assertStatementIsParentCall('__construct', 0);
-    $this->assertCallHasArgs([
+    $php_tester->assertStatementIsParentCall('__construct', 0);
+    $php_tester->assertCallHasArgs([
       'Plugin/CatFeeder' => 'string',
       'namespaces' => 'var',
       'module_handler' => 'var',
@@ -73,14 +74,14 @@ class ComponentPluginType8Test extends TestBaseComponentGeneration {
     ],
     '__construct', 0);
 
-    $this->assertStatementIsLocalMethodCall('alterInfo', '__construct', 1);
-    $this->assertCallHasArgs([
+    $php_tester->assertStatementIsLocalMethodCall('alterInfo', '__construct', 1);
+    $php_tester->assertCallHasArgs([
       'cat_feeder_info' => 'string',
     ],
     '__construct', 1);
 
-    $this->assertStatementIsLocalMethodCall('setCacheBackend', '__construct', 2);
-    $this->assertCallHasArgs([
+    $php_tester->assertStatementIsLocalMethodCall('setCacheBackend', '__construct', 2);
+    $php_tester->assertCallHasArgs([
       'cache_backend' => 'var',
       'cat_feeder_plugins' => 'string',
     ],
@@ -88,35 +89,31 @@ class ComponentPluginType8Test extends TestBaseComponentGeneration {
 
     // Check the annotation class file.
     $annotation_file = $files["src/Annotation/CatFeeder.php"];
-    $this->assertWellFormedPHP($annotation_file);
-    $this->assertDrupalCodingStandards($annotation_file);
-    $this->parseCode($annotation_file);
-    $this->assertHasClass('Drupal\test_module\Annotation\CatFeeder');
-    $this->assertClassHasParent('Drupal\Component\Annotation\Plugin');
-    $this->assertClassHasPublicProperty('id', 'string');
-    $this->assertClassHasPublicProperty('label', 'Drupal\Core\Annotation\Translation');
-    $this->assertClassDocBlockHasLine('Defines the Cat Feeder plugin annotation object.');
-    $this->assertClassDocBlockHasLine('Plugin namespace: CatFeeder.');
-    $this->assertClassDocBlockHasLine('@Annotation');
+
+    $php_tester = new PHPTester($annotation_file);
+    $php_tester->assertDrupalCodingStandards();
+    $php_tester->assertHasClass('Drupal\test_module\Annotation\CatFeeder');
+    $php_tester->assertClassHasParent('Drupal\Component\Annotation\Plugin');
+    $php_tester->assertClassHasPublicProperty('id', 'string');
+    $php_tester->assertClassHasPublicProperty('label', 'Drupal\Core\Annotation\Translation');
+    $php_tester->assertClassDocBlockHasLine('Defines the Cat Feeder plugin annotation object.');
+    $php_tester->assertClassDocBlockHasLine('Plugin namespace: CatFeeder.');
+    $php_tester->assertClassDocBlockHasLine('@Annotation');
 
     // Check the plugin base class file.
     $plugin_base_file = $files["src/Plugin/CatFeeder/CatFeederBase.php"];
-    $this->assertWellFormedPHP($plugin_base_file);
-    $this->assertDrupalCodingStandards($plugin_base_file);
-    $this->assertNoTrailingWhitespace($plugin_base_file);
 
-    $this->parseCode($plugin_base_file);
-    $this->assertHasClass('Drupal\test_module\Plugin\CatFeeder\CatFeederBase');
-    $this->assertClassHasInterfaces(['Drupal\test_module\Plugin\CatFeeder\CatFeederInterface']);
+    $php_tester = new PHPTester($plugin_base_file);
+    $php_tester->assertDrupalCodingStandards();
+    $php_tester->assertHasClass('Drupal\test_module\Plugin\CatFeeder\CatFeederBase');
+    $php_tester->assertClassHasInterfaces(['Drupal\test_module\Plugin\CatFeeder\CatFeederInterface']);
 
     // Check the plugin interface file.
     $plugin_interface_file = $files["src/Plugin/CatFeeder/CatFeederInterface.php"];
-    $this->assertWellFormedPHP($plugin_interface_file);
-    $this->assertDrupalCodingStandards($plugin_interface_file);
-    $this->assertNoTrailingWhitespace($plugin_interface_file);
 
-    $this->parseCode($plugin_interface_file);
-    $this->assertHasInterface('Drupal\test_module\Plugin\CatFeeder\CatFeederInterface');
+    $php_tester = new PHPTester($plugin_interface_file);
+    $php_tester->assertDrupalCodingStandards();
+    $php_tester->assertHasInterface('Drupal\test_module\Plugin\CatFeeder\CatFeederInterface');
 
     // Check the services file.
     $services_file = $files["$module_name.services.yml"];
@@ -163,29 +160,26 @@ class ComponentPluginType8Test extends TestBaseComponentGeneration {
 
     // Check the plugin manager file, as it mentions the interface.
     $plugin_manager_file = $files["src/CatFeederManager.php"];
-    $this->assertWellFormedPHP($plugin_manager_file);
-    $this->assertDrupalCodingStandards($plugin_manager_file);
-    $this->assertNoTrailingWhitespace($plugin_manager_file, "The plugin service class file contains no trailing whitespace.");
-    $this->assertClassFileFormatting($plugin_manager_file);
+
+    $php_tester = new PHPTester($plugin_manager_file);
+    $php_tester->assertDrupalCodingStandards();
+    $php_tester->assertHasClass('Drupal\test_module\CatFeederManager');
 
     // Check the files that go in the nested folder.
     // Check the plugin base class file.
     $plugin_base_file = $files["src/Plugin/Animals/CatFeeder/CatFeederBase.php"];
-    $this->assertWellFormedPHP($plugin_base_file);
-    $this->assertDrupalCodingStandards($plugin_base_file);
-    $this->assertNoTrailingWhitespace($plugin_base_file);
 
-    $this->parseCode($plugin_base_file);
-    $this->assertHasClass('Drupal\test_module\Plugin\Animals\CatFeeder\CatFeederBase');
-    $this->assertClassHasInterfaces(['Drupal\test_module\Plugin\Animals\CatFeeder\CatFeederInterface']);
+    $php_tester = new PHPTester($plugin_base_file);
+    $php_tester->assertDrupalCodingStandards();
+    $php_tester->assertHasClass('Drupal\test_module\Plugin\Animals\CatFeeder\CatFeederBase');
+    $php_tester->assertClassHasInterfaces(['Drupal\test_module\Plugin\Animals\CatFeeder\CatFeederInterface']);
 
     // Check the plugin interface file.
     $plugin_interface_file = $files["src/Plugin/Animals/CatFeeder/CatFeederInterface.php"];
-    $this->assertWellFormedPHP($plugin_interface_file);
-    $this->assertDrupalCodingStandards($plugin_interface_file);
-    $this->assertNoTrailingWhitespace($plugin_interface_file);
-    $this->parseCode($plugin_interface_file);
-    $this->assertHasInterface('Drupal\test_module\Plugin\Animals\CatFeeder\CatFeederInterface');
+
+    $php_tester = new PHPTester($plugin_interface_file);
+    $php_tester->assertDrupalCodingStandards();
+    $php_tester->assertHasInterface('Drupal\test_module\Plugin\Animals\CatFeeder\CatFeederInterface');
   }
 
 }
