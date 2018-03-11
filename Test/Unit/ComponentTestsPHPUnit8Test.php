d914cd0f202d6a3549784157b4af5a50454050e6
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-03-11 21:28:56 +0000

    Changed unit test test to use PHP helper.

--- Test/Unit/ComponentTestsPHPUnit8Test.php
+++ Test/Unit/ComponentTestsPHPUnit8Test.php
@@ -2,12 +2,13 @@
 
 namespace DrupalCodeBuilder\Test\Unit;
 
+use DrupalCodeBuilder\Test\Unit\Parsing\PHPTester;
 use DrupalCodeBuilder\Test\Unit\Parsing\YamlTester;
 
 /**
  * Tests the PHPUnit test class generator.
  */
-class ComponentTestsPHPUnit8Test extends TestBaseComponentGeneration {
+class ComponentTestsPHPUnit8Test extends TestBase {
 
   /**
    * The Drupal core major version to set up for this test.
@@ -17,11 +18,11 @@ class ComponentTestsPHPUnit8Test extends TestBaseComponentGeneration {
   protected $drupalMajorVersion = 8;
 
   /**
-   * The PHP CodeSniffer to exclude for this test.
+   * The PHP CodeSniffer rules to exclude for this test class files.
    *
    * @var string[]
    */
-  static protected $phpcsExcludedSniffs = [
+  protected $phpcsExcludedSniffs = [
     // This picks up that the setUp() merely calls the parent class, but this
     // is useful to developers as a starting point to add code to, therefore
     // this is excluded.
@@ -54,15 +55,11 @@ class ComponentTestsPHPUnit8Test extends TestBaseComponentGeneration {
 
     $test_file = $files["tests/src/MyTest.php"];
 
-    $this->assertWellFormedPHP($test_file);
-    $this->assertDrupalCodingStandards($test_file);
-    $this->assertNoTrailingWhitespace($test_file, "The test class file contains no trailing whitespace.");
-    $this->assertClassFileFormatting($test_file);
-
-    $this->parseCode($test_file);
-    $this->assertHasClass('Drupal\Tests\test_module\MyTest');
-    $this->assertHasMethods(['setUp', 'testMyTest']);
-    $this->assertClassHasPublicProperty('modules', 'array', ['system', 'user', 'test_module']);
+    $php_tester = new PHPTester($test_file);
+    $php_tester->assertDrupalCodingStandards($this->phpcsExcludedSniffs);
+    $php_tester->assertHasClass('Drupal\Tests\test_module\MyTest');
+    $php_tester->assertHasMethods(['setUp', 'testMyTest']);
+    $php_tester->assertClassHasPublicProperty('modules', 'array', ['system', 'user', 'test_module']);
   }
 
   /**
@@ -95,15 +92,11 @@ class ComponentTestsPHPUnit8Test extends TestBaseComponentGeneration {
 
     $test_file = $files["tests/src/MyTest.php"];
 
-    $this->assertWellFormedPHP($test_file);
-    $this->assertDrupalCodingStandards($test_file);
-    $this->assertNoTrailingWhitespace($test_file, "The test class file contains no trailing whitespace.");
-    $this->assertClassFileFormatting($test_file);
-
-    $this->parseCode($test_file);
-    $this->assertHasClass('Drupal\Tests\test_module\MyTest');
-    $this->assertHasMethods(['setUp', 'testMyTest']);
-    $this->assertClassHasPublicProperty('modules', 'array', [
+    $php_tester = new PHPTester($test_file);
+    $php_tester->assertDrupalCodingStandards($this->phpcsExcludedSniffs);
+    $php_tester->assertHasClass('Drupal\Tests\test_module\MyTest');
+    $php_tester->assertHasMethods(['setUp', 'testMyTest']);
+    $php_tester->assertClassHasPublicProperty('modules', 'array', [
       'system',
       'user',
       'dependency_one',
@@ -139,15 +132,11 @@ class ComponentTestsPHPUnit8Test extends TestBaseComponentGeneration {
 
     $test_file = $files["tests/src/Kernel/MyTest.php"];
 
-    $this->assertWellFormedPHP($test_file);
-    $this->assertDrupalCodingStandards($test_file);
-    $this->assertNoTrailingWhitespace($test_file, "The test class file contains no trailing whitespace.");
-    $this->assertClassFileFormatting($test_file);
-
-    $this->parseCode($test_file);
-    $this->assertHasClass('Drupal\Tests\test_module\Kernel\MyTest');
-    $this->assertClassHasParent('Drupal\KernelTests\KernelTestBase');
-    $this->assertHasMethods(['setUp', 'testMyTest']);
+    $php_tester = new PHPTester($test_file);
+    $php_tester->assertDrupalCodingStandards($this->phpcsExcludedSniffs);
+    $php_tester->assertHasClass('Drupal\Tests\test_module\Kernel\MyTest');
+    $php_tester->assertClassHasParent('Drupal\KernelTests\KernelTestBase');
+    $php_tester->assertHasMethods(['setUp', 'testMyTest']);
   }
 
   /**
@@ -217,33 +206,31 @@ class ComponentTestsPHPUnit8Test extends TestBaseComponentGeneration {
 
     // Check the main module plugin file.
     $plugin_file = $files["src/Plugin/Block/Alpha.php"];
-    $this->assertWellFormedPHP($plugin_file);
-    $this->assertDrupalCodingStandards($plugin_file);
 
-    $this->parseCode($plugin_file);
-    $this->assertHasClass('Drupal\generated_module\Plugin\Block\Alpha');
-    $this->assertClassHasParent('Drupal\Core\Block\BlockBase');
+    $php_tester = new PHPTester($plugin_file);
+    $php_tester->assertDrupalCodingStandards();
+    $php_tester->assertHasClass('Drupal\generated_module\Plugin\Block\Alpha');
+    $php_tester->assertClassHasParent('Drupal\Core\Block\BlockBase');
 
     // Check the test module plugin file.
     $test_plugin_file = $files["tests/modules/test_module/src/Plugin/Block/Alpha.php"];
-    $this->assertWellFormedPHP($test_plugin_file);
-    $this->assertDrupalCodingStandards($test_plugin_file);
 
-    $this->parseCode($test_plugin_file);
-    $this->assertHasClass('Drupal\test_module\Plugin\Block\Alpha');
-    $this->assertClassHasParent('Drupal\Core\Block\BlockBase');
+    $php_tester = new PHPTester($test_plugin_file);
+    $php_tester->assertDrupalCodingStandards();
+    $php_tester->assertHasClass('Drupal\test_module\Plugin\Block\Alpha');
+    $php_tester->assertClassHasParent('Drupal\Core\Block\BlockBase');
 
     $test_file = $files["tests/src/MyTest.php"];
 
-    $this->parseCode($test_file);
-    $this->assertHasClass('Drupal\Tests\generated_module\MyTest');
+    $php_tester = new PHPTester($test_file);
+    $php_tester->assertHasClass('Drupal\Tests\generated_module\MyTest');
     $expected_modules_property_value = [
       'system',
       'user',
       'generated_module',
       'test_module',
     ];
-    $this->assertClassHasPublicProperty('modules', 'array', $expected_modules_property_value);
+    $php_tester->assertClassHasPublicProperty('modules', 'array', $expected_modules_property_value);
   }
 
 }
