ae507d8541a2b3fa10ab4823fb2e3ac0f8eb8c22
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-03-25 21:05:44 +0100

    Added test for YAML plugin generator.

---
+++ Test/Unit/ComponentPluginsYAML8Test.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace DrupalCodeBuilder\Test\Unit;
+
+use \DrupalCodeBuilder\Exception\InvalidInputException;
+use DrupalCodeBuilder\Test\Unit\Parsing\PHPTester;
+use DrupalCodeBuilder\Test\Unit\Parsing\YamlTester;
+
+/**
+ * Tests the PluginYAML generator class.
+ *
+ * @group yaml
+ */
+class ComponentPluginsYAML8Test extends TestBase {
+
+  /**
+   * The Drupal core major version to set up for this test.
+   *
+   * @var int
+   */
+  protected $drupalMajorVersion = 8;
+
+  /**
+   * Test PluginYAML component.
+   */
+  function testBasicYAMLPluginsGeneration() {
+    // Create a module.
+    $module_name = 'test_module';
+    $module_data = array(
+      'base' => 'module',
+      'root_name' => $module_name,
+      'readable_name' => 'Test module',
+      'short_description' => 'Test Module description',
+      'hooks' => [],
+      'plugins_yaml' => [
+        0 => [
+          'plugin_type' => 'menu.link',
+          'plugin_name' => 'alpha',
+        ],
+        1 => [
+          'plugin_type' => 'menu.link',
+          'plugin_name' => 'beta',
+        ],
+      ],
+      'readme' => FALSE,
+    );
+    $files = $this->generateModuleFiles($module_data);
+    $file_names = array_keys($files);
+
+    $this->assertCount(2, $files, "Expected number of files is returned.");
+    $this->assertArrayHasKey("$module_name.info.yml", $files, "The files list has a .info.yml file.");
+    $this->assertArrayHasKey("$module_name.links.menu.yml", $files, "The files list has a YAML plugin file.");
+
+    // Check the plugin file.
+    $plugin_file = $files["$module_name.links.menu.yml"];
+
+    $yaml_tester = new YamlTester($plugin_file);
+    $yaml_tester->assertHasProperty('test_module.alpha');
+  }
+
+  /**
+   * Test PluginYAML component with annotated plugins too.
+   */
+  function testYAMLPluginsGenerationWithAnnotated() {
+    // Create a module.
+    $module_name = 'test_module';
+    $module_data = array(
+      'base' => 'module',
+      'root_name' => $module_name,
+      'readable_name' => 'Test module',
+      'short_description' => 'Test Module description',
+      'hooks' => [],
+      'plugins_yaml' => [
+        0 => [
+          'plugin_type' => 'menu.link',
+          'plugin_name' => 'alpha',
+        ],
+        1 => [
+          'plugin_type' => 'menu.link',
+          'plugin_name' => 'beta',
+        ],
+      ],
+      'plugins' => [
+        0 => [
+          'plugin_type' => 'block',
+          'plugin_name' => 'alpha',
+        ],
+      ],
+      'readme' => FALSE,
+    );
+    $files = $this->generateModuleFiles($module_data);
+
+    $this->assertCount(4, $files, "Expected number of files is returned.");
+    $this->assertArrayHasKey("$module_name.info.yml", $files, "The files list has a .info.yml file.");
+    $this->assertArrayHasKey("$module_name.links.menu.yml", $files, "The files list has a YAML plugin file.");
+    $this->assertArrayHasKey("src/Plugin/Block/Alpha.php", $files, "The files list has a plugin file.");
+    $this->assertArrayHasKey("config/schema/test_module.schema.yml", $files, "The files list has a schema file.");
+  }
+
+}
