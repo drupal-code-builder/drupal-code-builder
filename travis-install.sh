560b24be1c28c3628e0e768c695d25f954f4d80b
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-04-12 22:18:27 +0100

    move setup commands to a helper file

---
+++ travis-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$TESTTYPE" = "dcb" ]; then
+  composer install;
+fi
+if [ "$TESTTYPE" = "drupal" ]; then
+  # Remove xdebug. We aren't generating code coverage, and it slows down Composer.
+  phpenv config-rm xdebug.ini || true
+
+  mysql -e 'create database entity'
+  # Export database variable for kernel tests.
+  export SIMPLETEST_DB=mysql://root:@127.0.0.1/entity
+  # Download Drupal 8 core.
+  travis_retry git clone --branch 8.5.x --depth 1 http://git.drupal.org/project/drupal.git
+  cd drupal
+  composer self-update
+  composer install -n
+
+  # Reference DCB in build site.
+  ln -s $TESTDIR vendor/drupal-code-builder
+  composer require joachim-n/case-converter
+fi
