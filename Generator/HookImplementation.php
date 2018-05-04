3e4386639783e94e7f3bad10b2f1d93a61a848ae
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-05-04 20:49:44 +0100

    Added declaration of properties for HookImplementation generator.

--- Generator/HookImplementation.php
+++ Generator/HookImplementation.php
@@ -2,6 +2,8 @@
 
 namespace DrupalCodeBuilder\Generator;
 
+use DrupalCodeBuilder\Utility\InsertArray;
+
 /**
  * Generator for a single hook implementation.
  *
@@ -10,28 +12,20 @@ namespace DrupalCodeBuilder\Generator;
 class HookImplementation extends PHPFunction {
 
   /**
-   * Constructor.
-   *
-   * TODO: remove this once the data properties are properly declared.
-   *
-   * @param $component_name
-   *  The name of a function component should be its function (or method) name.
-   * @param $component_data
-   *   An array of data for the component. The following properties are
-   *   required:
-   *     - 'hook_name': The full name of the hook.
-   *     - 'code_file': The name of the file this hook should be placed in, with
-   *        tokens.
-   */
-  function __construct($component_name, $component_data, $root_generator) {
-    parent::__construct($component_name, $component_data, $root_generator);
-  }
-
-  /**
    * {@inheritdoc}
    */
   public static function componentDataDefinition() {
     $properties = parent::componentDataDefinition();
+
+    $hook_name_property = [
+      // The full name of the hook.
+      'hook_name' => [
+        'internal' => TRUE,
+      ],
+    ];
+    // Insert the hook_name property before the doxygen_first property, as that
+    // depends on it.
+    InsertArray::insertBefore($properties, 'doxygen_first', $hook_name_property);
 
     $properties['doxygen_first']['default'] = function($component_data) {
       return "Implements {$component_data['hook_name']}().";
