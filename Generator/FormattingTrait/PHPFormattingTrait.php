2dec7d44a610cdecbbf70bda11756e25cc6397d2
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-05-08 20:41:14 +0100

    Added parameter for indentation level to indentCodeLines().

--- Generator/FormattingTrait/PHPFormattingTrait.php
+++ Generator/FormattingTrait/PHPFormattingTrait.php
@@ -41,13 +41,18 @@ trait PHPFormattingTrait {
    *
    * @param array $lines
    *   An array of code lines.
+   * @param int $indent
+   *   (optional) The number of indentation levels to add. Defaults to 1, that
+   *   is, an indentation of two spaces.
    *
    * @return
    *   The array of code lines with the indentation applied.
    */
-  function indentCodeLines($lines) {
-    $indented_lines = array_map(function ($line) {
-      return empty($line) ? $line : '  ' . $line;
+  function indentCodeLines($lines, $indent = 1) {
+    $indent = str_repeat('  ', $indent);
+
+    $indented_lines = array_map(function ($line) use ($indent) {
+      return empty($line) ? $line : $indent . $line;
     }, $lines);
     return $indented_lines;
   }
