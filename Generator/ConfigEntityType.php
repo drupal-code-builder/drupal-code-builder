be5ea43e922cf36c86acad49eada4c4d0cef121b
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-05-09 14:34:24 +0100

    Added form elements to config entity type form handler.

--- Generator/ConfigEntityType.php
+++ Generator/ConfigEntityType.php
@@ -136,6 +136,83 @@ class ConfigEntityType extends EntityTypeBase {
       }
     }
 
+    // Add form elements for the form handlers, if present.
+    $form_handlers_to_add_form_elements_to = [];
+    if (isset($components['handler_form_default'])) {
+      $form_handlers_to_add_form_elements_to[] = 'handler_form_default';
+    }
+    else {
+      // Only add form elements to the add and edit form handlers if they are
+      // not inheriting from the default form handler.
+      foreach (['form_add', 'form_edit'] as $form_handler_key) {
+        $data_key = "handler_{$form_handler_key}";
+        if (isset($components[$data_key])) {
+          $form_handlers_to_add_form_elements_to[] = $data_key;
+        }
+      }
+    }
+
+    foreach ($form_handlers_to_add_form_elements_to as $data_key) {
+      $pseudo_form_id = $this->component_data['entity_type_id'] . '_' . $data_key;
+
+      // Special handling for the id and label properties.
+      $components[$data_key . ':label'] = [
+        'component_type' => 'FormElement',
+        'containing_component' => "%requester:{$data_key}:form",
+        'pseudo_form_id' => $pseudo_form_id,
+        'form_key' => 'label',
+        'element_type' => 'textfield',
+        'element_title' => "Name",
+        'element_description' => "The human-readable name of this entity",
+        'element_array' => [
+          'default_value' => "£this->entity->get('label')",
+        ],
+      ];
+
+      $components[$data_key . ':id'] = [
+        'component_type' => 'FormElement',
+        'containing_component' => "%requester:{$data_key}:form",
+        'pseudo_form_id' => $pseudo_form_id,
+        'form_key' => 'id',
+        'element_type' => 'machine_name',
+        'element_title' => '',
+        'element_description' => "A unique machine-readable name for this entity. It must only contain lowercase letters, numbers, and underscores.",
+        'element_array' => [
+          'default_value' => "£this->entity->id()",
+          'machine_name' => [
+            'exists' => "['{$this->component_data['qualified_class_name']}', 'load']",
+            'source' => "['label']",
+          ],
+        ],
+      ];
+      if (isset($this->component_data['bundle_of_entity'])) {
+        $components[$data_key . ':id']['element_array']['maxlength'] =
+          '\Drupal\Core\Entity\EntityTypeInterface::BUNDLE_MAX_LENGTH';
+      }
+
+      // Add a form element for each custom entity property.
+      foreach ($this->component_data['entity_properties'] as $schema_item) {
+        $property_name = $schema_item['name'];
+
+        // Skip id and label; done above.
+        if ($property_name == 'id' || $property_name == 'label') {
+          continue;
+        }
+
+        $components[$data_key . ':' . $property_name] = [
+          'component_type' => 'FormElement',
+          'containing_component' => "%requester:{$data_key}:form",
+          'pseudo_form_id' => $pseudo_form_id,
+          'form_key' => $property_name,
+          'element_type' => 'textfield',
+          'element_title' => $schema_item['label'],
+          'element_array' => [
+            'default_value' => "£this->entity->get('{$property_name}')",
+          ],
+        ];
+      }
+    }
+
     $entity_config_key = $this->component_data['entity_type_id'];
     $module = $this->component_data['root_component_name'];
 
