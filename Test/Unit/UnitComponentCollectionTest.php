0a4f90a062c6bc1aa9098006fca33e50304c1a3d
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-05-14 21:17:50 +0100

    Added testing of component collection unique ID enforcement.

--- Test/Unit/UnitComponentCollectionTest.php
+++ Test/Unit/UnitComponentCollectionTest.php
@@ -13,6 +13,29 @@ use DrupalCodeBuilder\Generator\RootComponent;
 class UnitComponentCollectionTest extends TestCase {
 
   /**
+   * Tests components can't be added with an existing unique ID.
+   */
+  public function testDuplicateUniqueID() {
+    $collection = new ComponentCollection;
+
+    $root_component = $this->prophesize(RootComponent::class);
+    $root_component->getUniqueID()->willReturn('root:root');
+    $root_component->containingComponent()->willReturn(NULL);
+    $collection->addComponent('root', $root_component->reveal(), NULL);
+
+    $component_two = $this->prophesize(BaseGenerator::class);
+    $component_two->getUniqueID()->willReturn('not_so_unique');
+    $collection->addComponent('component_2', $component_two->reveal(), NULL);
+
+    $component_three = $this->prophesize(BaseGenerator::class);
+    $component_three->getUniqueID()->willReturn('not_so_unique');
+
+    $this->expectException(\Exception::class);
+
+    $collection->addComponent('component_3', $component_three->reveal(), NULL);
+  }
+
+  /**
    * Tests components can't be added to the collection after the tree is built.
    */
   public function testNoFurtherComponentsAfterTreeBuild() {
