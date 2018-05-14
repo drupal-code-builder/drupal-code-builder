f14ce24e70c62c0d8354ec11e636a3cb70a8bbc3
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-05-14 21:16:22 +0100

    Added unit test for ComponentCollection.

---
+++ Test/Unit/UnitComponentCollectionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DrupalCodeBuilder\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+use DrupalCodeBuilder\Generator\Collection\ComponentCollection;
+use DrupalCodeBuilder\Generator\BaseGenerator;
+use DrupalCodeBuilder\Generator\RootComponent;
+
+/**
+ * Unit tests for the ComponentCollection class.
+ */
+class UnitComponentCollectionTest extends TestCase {
+
+  /**
+   * Tests components can't be added to the collection after the tree is built.
+   */
+  public function testNoFurtherComponentsAfterTreeBuild() {
+    $collection = new ComponentCollection;
+
+    $root_component = $this->prophesize(RootComponent::class);
+    $root_component->getUniqueID()->willReturn('root:root');
+    $root_component->containingComponent()->willReturn(NULL);
+
+    $collection->addComponent('root', $root_component->reveal(), NULL);
+
+    $collection->assembleContainmentTree();
+
+    $root_requested = $this->prophesize(BaseGenerator::class);
+
+    $this->expectException(\LogicException::class);
+
+    $collection->addComponent('requested', $root_requested->reveal(), $root_component->reveal());
+  }
+
+}
