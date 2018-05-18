f1eac6510914dcd4d56035ac93d3aad12ab5d0df
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-05-18 21:10:38 +0100

    Changed PHPMethod generator to deprecated.

--- Generator/PHPMethod.php
+++ Generator/PHPMethod.php
@@ -4,6 +4,10 @@ namespace DrupalCodeBuilder\Generator;
 
 /**
  * Generator class for class methods.
+ *
+ * @deprecated
+ *
+ * TODO: replace this with PHPFunction throughout.
  */
 class PHPMethod extends PHPFunction {
 
