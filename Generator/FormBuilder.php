f50c42ba2049b7121a41457d8fe14fe2af2be13f
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-05-18 21:05:49 +0100

    Changed FormBuilder to use indentation helper.

--- Generator/FormBuilder.php
+++ Generator/FormBuilder.php
@@ -28,25 +28,30 @@ class FormBuilder extends PHPMethod {
 
     // The function name for a form builder is not fixed: normal forms use
     // form() but entity form handlers use buildForm().
-    $function_code[] = "  £{$this->name} = parent::form(£form, £form_state);";
-    $function_code[] = '';
+    $body_code = [];
+    $body_code[] = "£{$this->name} = parent::form(£form, £form_state);";
+    $body_code[] = '';
 
     foreach ($children_contents as $child_item) {
       $content = $child_item['content'];
 
-      $function_code[] = "  £form['{$content['key']}'] = [";
+      $body_code[] = "£form['{$content['key']}'] = [";
 
       // Render the FormAPI element recursively.
       $form_renderer = new \DrupalCodeBuilder\Generator\Render\FormAPIArrayRenderer($content['array']);
       $element_lines = $form_renderer->render();
-      $element_lines = $this->indentCodeLines($element_lines, 2);
-      $function_code = array_merge($function_code, $element_lines);
+      $element_lines = $this->indentCodeLines($element_lines);
+      $body_code = array_merge($body_code, $element_lines);
 
-      $function_code[] = '  ];';
+      $body_code[] = '];';
     }
 
-    $function_code[] = '';
-    $function_code[] = '  return £form;';
+    $body_code[] = '';
+    $body_code[] = 'return £form;';
+
+    $body_code = $this->indentCodeLines($body_code);
+
+    $function_code = array_merge($function_code, $body_code);
 
     $function_code[] = "}";
 
