3ff784e18c91e0cbcc2abc41cc0a839ac96bec59
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2018-07-26 07:12:42 +0100

    Added test analysis data for admin routes.

---
+++ Test/sample_hook_definitions/8/admin_routes_processed.php
@@ -0,0 +1,15 @@
+<?php $data =
+array (
+  'system.admin_config_development' =>
+  array (
+    'route_name' => 'system.admin_config_development',
+    'path' => '/admin/config/development',
+    'title' => 'Development',
+  ),
+  'system.admin_config_system' =>
+  array (
+    'route_name' => 'system.admin_config_system',
+    'path' => '/admin/config/system',
+    'title' => 'System',
+  ),
+);
