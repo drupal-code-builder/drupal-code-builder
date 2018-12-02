drupal-code-builder/drupal-code-builder

commit 077cd14ab5b626f630b906832918fcda7a090304

Removed pointless merging of incremental analysis data.

Task/Collect.php
@@ -120,11 +120,6 @@ class Collect extends Base {
 
       $jobs_data = $collector_helper->collect($jobs);
 
-      $incremental_data[$collector_class_name] = $collector_helper->mergeComponentData(
-        $incremental_data[$collector_class_name],
-        $jobs_data
-      );
-
       $last_job = end($jobs);
       if (!empty($last_job['last'])) {
         $final_jobs[$collector_class_name] = TRUE;

commit 3efc0e279e7e557f4888969356e6fb01ca5ec746

Fixed analysis data not being saved - bug introduced in 077cd14ab5b626f630b906832918fcda7a090304.

Task/Collect.php
@@ -118,7 +118,7 @@ class Collect extends Base {
     foreach ($grouped_jobs as $collector_class_name => $jobs) {
       $collector_helper = $this->getHelper($collector_class_name);
 
-      $jobs_data = $collector_helper->collect($jobs);
+      $incremental_data[$collector_class_name] = $collector_helper->collect($jobs);
 
       $last_job = end($jobs);
       if (!empty($last_job['last'])) {