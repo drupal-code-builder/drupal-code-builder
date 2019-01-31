0230b18ed565656d7b3512ab26b65190d3f3328d
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2019-01-31 14:34:28 +0000

    WIP!

--- Generator/DrushCommand.php
+++ Generator/DrushCommand.php
@@ -2,6 +2,8 @@
 
 namespace DrupalCodeBuilder\Generator;
 
+use CaseConverter\CaseString;
+
 /**
  * Generator for a Drush 9 command.
  */
@@ -13,12 +15,18 @@ class DrushCommand extends PHPClassFileWithInjection {
   public static function componentDataDefinition() {
     $data_definition = array(
       'command_class_name' => array(
-        'label' => 'Form class name',
+        'label' => 'Command class name',
         'required' => TRUE,
         'processing' => function($value, &$component_data, $property_name, &$property_info) {
           $component_data['command_class_name'] = ucfirst($value);
         },
       ),
+      'drush_service_name' => [
+        'internal' => TRUE,
+        'default' => function($component_data) {
+          return $component_data['root_component_name'] . '.' . CaseString::pascal($component_data['command_class_name'])->snake();
+        },
+      ],
       'injected_services' => array(
         'label' => 'Injected services',
         'format' => 'array',
@@ -45,6 +53,76 @@ class DrushCommand extends PHPClassFileWithInjection {
     $data_definition['parent_class_name']['default'] = '\Drush\Commands\DrushCommands';
 
     return $data_definition;
+  }
+
+  /**
+   * Return an array of subcomponent types.
+   */
+  public function requiredComponents() {
+    $components = [];
+
+    $yaml_data_arguments = [];
+    foreach ($this->component_data['injected_services'] as $service_id) {
+      $components['service_' . $service_id] = array(
+        'component_type' => 'InjectedService',
+        'containing_component' => '%requester',
+        'service_id' => $service_id,
+      );
+
+      // Add the service ID to the arguments in the YAML data.
+      $yaml_data_arguments[] = '@' . $service_id;
+    }
+
+    $yaml_service_definition = [
+      'class' => $this->component_data['qualified_class_name'],
+    ];
+    if ($yaml_data_arguments) {
+      $yaml_service_definition['arguments'] = $yaml_data_arguments;
+    }
+
+    dump($yaml_service_definition);
+
+    /*
+    services:
+  config_devel.commands:
+    class: \Drupal\config_devel\Commands\ConfigDevelCommands
+    arguments:
+      - '@module_handler'
+      - '@theme_handler'
+      - '@info_parser'
+      - '@config.factory'
+      - '@config_devel.writeback_subscriber'
+      - '@config_devel.auto_import_subscriber'
+      - '@file_system'
+    tags:
+      - { name: drush.command }
+    */
+
+    // Service tags.
+    $yaml_service_definition['tags'][] = [
+      'name' => 'drush.command',
+    ];
+
+    $yaml_data = [];
+    $yaml_data['services'] = [
+      $this->component_data['drush_service_name'] => $yaml_service_definition,
+    ];
+
+    $components['drush.services.yml'] = [
+      'component_type' => 'YMLFile',
+      'filename' => 'drush.services.yml',
+      'yaml_data' => $yaml_data,
+      'yaml_inline_level' => 3,
+      // Expand the tags property further than the others.
+      'inline_levels_extra' => [
+        'tags' => [
+          'address' => ['services', '*', 'tags'],
+          'level' => 4,
+        ],
+      ],
+    ];
+
+    return $components;
   }
 
 }
