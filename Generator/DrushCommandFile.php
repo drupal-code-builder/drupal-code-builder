8a74b62a8d2c6ee10fc430604ee4ddaa7ac1aa86
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2019-02-01 16:37:59 +0000

    acquire DI

--- Generator/DrushCommandFile.php
+++ Generator/DrushCommandFile.php
@@ -10,10 +10,36 @@ use CaseConverter\CaseString;
 class DrushCommandFile extends PHPClassFileWithInjection {
 
   /**
+   * Define the component data this component needs to function.
+   */
+  public static function componentDataDefinition() {
+    $data_definition = parent::componentDataDefinition();
+
+    $data_definition += [
+      'injected_services' => [
+        'acquired' => TRUE,
+        'format' => 'array',
+      ],
+    ];
+
+    return $data_definition;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function getMergeTag() {
     return 'drush-commands';
+  }
+
+  /**
+   * Return an array of subcomponent types.
+   */
+  public function requiredComponents() {
+    // dump($this->component_data);
+    $components = [];
+
+    return $components;
   }
 
 }
