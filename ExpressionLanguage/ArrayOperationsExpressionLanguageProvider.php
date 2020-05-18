48685b71f931ac987bbf852943e58c785dd14214
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2020-05-18 23:20:18 +0100

    argh more EL functions.

--- ExpressionLanguage/ArrayOperationsExpressionLanguageProvider.php
+++ ExpressionLanguage/ArrayOperationsExpressionLanguageProvider.php
@@ -58,6 +58,31 @@ class ArrayOperationsExpressionLanguageProvider implements ExpressionFunctionPro
           $pieces = explode('\\', $qualified_class_name);
           return end($pieces);
        }),
+      new ExpressionFunction(
+        'pathFromQualifiedClassNamePieces',
+        function (array $qualified_class_name_pieces) {},
+        function ($arguments, array $qualified_class_name_pieces) {
+          // Lop off the initial Drupal\module and the final class name to
+          // build the path.
+          $path_pieces = array_slice($qualified_class_name_pieces, 2, -1);
+          // Add the initial src to the front.
+          array_unshift($path_pieces, 'src');
+          dump($path_pieces);
+          // crash();
+
+          return implode('/', $path_pieces);
+      }),
+      new ExpressionFunction(
+        'namespaceFromPieces',
+        function (array $qualified_class_name_pieces) {
+        },
+        function ($arguments, array $qualified_class_name_pieces) {
+          // Lop off the the final class name.
+          $path_pieces = array_slice($qualified_class_name_pieces, 0, -1);
+
+          return implode('\\', $path_pieces);
+        }
+      ),
     ];
   }
 
