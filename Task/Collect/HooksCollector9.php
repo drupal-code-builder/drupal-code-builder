fdefbbb53f53e1aec6f2df4de4615f6ca6fc3a23
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2020-06-30 22:03:52 +0100

    Added hooks collector for Drupal 9.

---
+++ Task/Collect/HooksCollector9.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DrupalCodeBuilder\Task\Collect;
+
+use DrupalCodeBuilder\Environment\EnvironmentInterface;
+
+/**
+ * Task helper for collecting data on hooks on Drupal 9.
+ */
+class HooksCollector9 extends HooksCollector8 {
+
+  // Identical to Drupal 8.
+
+}
