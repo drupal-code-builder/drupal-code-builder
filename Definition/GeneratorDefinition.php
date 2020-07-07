ca613d0546d4da8878996f73365733808eb566a1
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2020-07-07 21:51:53 +0100

    argh

--- Definition/GeneratorDefinition.php
+++ Definition/GeneratorDefinition.php
@@ -53,6 +53,19 @@ class GeneratorDefinition extends PropertyDefinition {
     return new static($data_type, $generator_type);
   }
 
+  static public function createFromGeneratorTypeWithConversion(string $generator_type, string $data_type = 'complex'): self {
+    $definition = new static($data_type, $generator_type);
+
+    $generate_task = \DrupalCodeBuilder\Factory::getTask('Generate', 'module');
+    // ARGH we're calling a method meant for the ROOT only!
+    $array_property_info = $generate_task->getComponentDataInfo($generator_type, TRUE);
+
+    \DrupalCodeBuilder\Generator\BaseGenerator::addArrayPropertyInfoToDefinition($definition, $array_property_info);
+
+    return $definition;
+  }
+
+
   public function getComponentType() :string {
     return $this->componentType;
   }
