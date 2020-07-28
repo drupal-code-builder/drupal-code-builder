66dc58ac0a0aa2551e287656ed4ed35b6a26306b
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2020-07-28 07:42:49 +0100

    throw if no root component name.

--- ExpressionLanguage/AcquisitionExpressionLanguageProvider.php
+++ ExpressionLanguage/AcquisitionExpressionLanguageProvider.php
@@ -20,11 +20,29 @@ class AcquisitionExpressionLanguageProvider implements ExpressionFunctionProvide
         function ($requester) {},
         function ($arguments, DataItem $requester) {
           if ($requester->hasProperty('root_name')) {
-            return $requester->root_name->value;
+            $return = $requester->root_name->value;
+
+            // Getting the root component name should always result in a value,
+            // so complain if not.
+            if (empty($return)) {
+              throw new \Exception(sprintf("Got empty root_name from requester %s",
+                $requester->getAddress()
+              ));
+            }
           }
           else {
-            return $requester->root_component_name->value;
+            $return = $requester->root_component_name->value;
+
+            if (empty($return)) {
+              // dump($requester);
+              throw new \Exception(sprintf(
+                "Got empty root_component_name from requester %s",
+                $requester->getAddress()
+              ));
+            }
           }
+
+          return $return;
        }),
     ];
   }
