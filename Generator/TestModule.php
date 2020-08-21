406fe6ef4134cab85c0310897ad1b4c93b7f70fc
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2020-08-21 08:48:58 +0100

    WIP test module default name

--- Generator/TestModule.php
+++ Generator/TestModule.php
@@ -4,6 +4,7 @@ namespace DrupalCodeBuilder\Generator;
 
 use CaseConverter\CaseString;
 use DrupalCodeBuilder\Definition\PropertyDefinition;
+use MutableTypedData\Definition\DefaultDefinition;
 
 /**
  * Generator for a module that is used for testing.
@@ -18,12 +19,6 @@ class TestModule extends Module {
    * Define the component data this component needs to function.
    */
   public static function componentDataDefinition() {
-    // $component_data_definition['test_class_name'] = [
-    //   // plain_class_name
-    //   'acquired' => TRUE,
-    // ];
-
-    // Put the parent definitions after ours.
     $component_data_definition = parent::componentDataDefinition();
 
     // Remove properties for components that test modules don't need.
@@ -37,8 +32,24 @@ class TestModule extends Module {
       unset($component_data_definition[$property]);
     }
 
+    // Acuisition hack to work in the UI stage.
+    $component_data_definition['test_class_name'] = PropertyDefinition::create('string')
+      ->setInternal(TRUE)
+      // ->setCallableDefault(function ($component_data) {
+      //   dump($component_data->getAddress());
+      //   dump($component_data->getItem('..:..:..:plain_class_name')->getAddress());
+      // });
+
+      ->setExpressionDefault("get('..:..:..:plain_class_name')");
+
     // Note that a default is provided for the root_name property in the
-    // requesting generator PHPUnitTest.
+    // requesting generator PHPUnitTest. TODO no here!
+    $component_data_definition['root_name']->setDefault(
+      DefaultDefinition::create()
+        ->setExpression("get('..:test_class_name') ~ '_cake'")
+        ->setDependencies('..:test_class_name')
+    );
+    // CaseString::pascal($component_data['test_class_name'])->snake()
 
     // The package is always 'Testing' for test modules, so set this to
     // computed.
