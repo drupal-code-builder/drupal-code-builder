09972336d20d6d59408769b5d2b4bcfb1160d3fc
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2020-10-06 10:05:25 +0100

    Fixed Drupal 5 hooks collector task parent class.

--- Task/Collect/HooksCollector5.php
+++ Task/Collect/HooksCollector5.php
@@ -7,7 +7,7 @@ use DrupalCodeBuilder\Environment\EnvironmentInterface;
 /**
  * Task helper for collecting data on hooks on Drupal 5.
  */
-class HooksCollector5 extends HooksCollector {
+class HooksCollector5 extends HooksCollector6 {
 
   // Drupal 5 version is the same as Drupal 6.
 
