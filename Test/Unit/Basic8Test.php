c2d59e65694d248a5598240bc954aa5d3bf0362c
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2020-10-13 22:37:59 +0100

    Added basic Drupal 8 test.

---
+++ Test/Unit/Basic8Test.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DrupalCodeBuilder\Test\Unit;
+
+/**
+ * Basic test class.
+ *
+ * @group pass
+ */
+class Basic8Test extends TestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $drupalMajorVersion = 8;
+
+  /**
+   * Test the hook data is reported correctly.
+   */
+  public function testReportHookData() {
+    $hooks_directory = \DrupalCodeBuilder\Factory::getEnvironment()->getDataDirectory();
+
+    $mb_task_handler_report = \DrupalCodeBuilder\Factory::getTask('ReportHookData');
+    $this->assertTrue(is_object($mb_task_handler_report), "A task handler object was returned.");
+
+    $hook_groups = $mb_task_handler_report->listHookData();
+    $this->assertTrue(is_array($hook_groups) || !empty($hook_groups), "An non-empty array of hook data was returned.");
+  }
+
+}
