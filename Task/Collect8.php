2d47125e08a7a6f24106823db16caed0a9db1568
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2020-10-13 20:18:36 +0100

    Changed to use injected helpers.

--- Task/Collect8.php
+++ Task/Collect8.php
@@ -25,21 +25,6 @@ use DrupalCodeBuilder\Task\Collect\AdminRoutesCollector;
 class Collect8 extends Collect {
 
   /**
-   * The short names of classes in this namespace that are collectors.
-   *
-   * @var string[]
-   */
-  protected $collectorClassNames = [
-    'HooksCollector',
-    'PluginTypesCollector',
-    'ServicesCollector',
-    'ServiceTagTypesCollector',
-    'FieldTypesCollector',
-    'DataTypesCollector',
-    'AdminRoutesCollector',
-  ];
-
-  /**
    * Constructor.
    */
   function __construct(
@@ -63,57 +48,6 @@ class Collect8 extends Collect {
       'Collect\DataTypesCollector' => $data_types_collector,
       'Collect\AdminRoutesCollector' => $admin_routes_collector,
     ];
-  }
-
-  /**
-   * Returns the helper for the given short class name.
-   *
-   * @param $class
-   *   The short class name.
-   *
-   * @return
-   *   The helper object.
-   */
-  protected function getHelper($class) {
-    if (!isset($this->helpers[$class])) {
-      $qualified_class = '\DrupalCodeBuilder\Task\Collect\\' . $class;
-
-      switch ($class) {
-        case 'HooksCollector':
-          $qualified_class .= $this->environment->getCoreMajorVersion();
-          $helper = new $qualified_class($this->environment);
-          break;
-        case 'PluginTypesCollector':
-          $helper = new $qualified_class(
-            $this->environment,
-            $this->getHelper('ContainerBuilderGetter'),
-            $this->getHelper('MethodCollector'),
-            $this->getHelper('CodeAnalyser')
-          );
-          break;
-        case 'ServiceTagTypesCollector':
-          $helper = new $qualified_class(
-            $this->environment,
-            $this->getHelper('ContainerBuilderGetter'),
-            $this->getHelper('MethodCollector')
-          );
-          break;
-        case 'ServicesCollector':
-          $helper = new $qualified_class(
-            $this->environment,
-            $this->getHelper('ContainerBuilderGetter'),
-            $this->getHelper('CodeAnalyser')
-          );
-          break;
-        default:
-          $helper = new $qualified_class($this->environment);
-          break;
-      }
-
-      $this->helpers[$class] = $helper;
-    }
-
-    return $this->helpers[$class];
   }
 
 }
