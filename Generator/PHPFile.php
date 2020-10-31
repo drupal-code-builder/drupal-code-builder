e34a245bed26d51923968d0e97bd1d7340c3ed50
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2020-10-31 07:44:19 +0000

    TODO

--- Generator/PHPFile.php
+++ Generator/PHPFile.php
@@ -194,7 +194,7 @@ class PHPFile extends File {
     $imports = [];
 
     if ($imported_classes) {
-      // Sort the imported classes.
+      // Sort the imported classes. todo sort AFTER removing the \\ !!
       sort($imported_classes);
 
       foreach ($imported_classes as $fully_qualified_class_name) {
