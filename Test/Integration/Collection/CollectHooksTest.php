d63df16cc43efcf58df27162a7174bcb483c22ee
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2020-11-02 07:01:20 +0000

    Removed autoloader workaround for Travis, no longer needed.

--- Test/Integration/Collection/CollectHooksTest.php
+++ Test/Integration/Collection/CollectHooksTest.php
@@ -23,11 +23,6 @@ class CollectHooksTest extends KernelTestBase {
    * {@inheritdoc}
    */
   protected function setUp() {
-    // Drupal doesn't know about DCB, so won't have it in its autoloader, so
-    // rely on the Factory file's autoloader.
-    $dcb_root = dirname(dirname(dirname(__DIR__)));
-    require_once("$dcb_root/Factory.php");
-
     \DrupalCodeBuilder\Factory::setEnvironmentLocalClass('DrupalLibrary')
       ->setCoreVersionNumber(\Drupal::VERSION);
 
