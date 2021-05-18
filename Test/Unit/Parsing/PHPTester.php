d79e76ce494ab5515e17965ff5f06ec55a8edc33
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2021-05-18 12:51:37 +0100

    Updated use of node names which are now Identifier objects.

--- Test/Unit/Parsing/PHPTester.php
+++ Test/Unit/Parsing/PHPTester.php
@@ -324,22 +324,22 @@ class PHPTester {
             $this->nodes['imports'][] = $node;
             break;
           case \PhpParser\Node\Stmt\Class_::class:
-            $this->nodes['classes'][$node->name] = $node;
+            $this->nodes['classes'][$node->name->toString()] = $node;
             break;
           case \PhpParser\Node\Stmt\Interface_::class:
-            $this->nodes['interfaces'][$node->name] = $node;
+            $this->nodes['interfaces'][$node->name->toString()] = $node;
             break;
           case \PhpParser\Node\Stmt\Property::class:
-            $this->nodes['properties'][$node->props[0]->name] = $node;
+            $this->nodes['properties'][$node->props[0]->name->toString()] = $node;
             break;
           case \PhpParser\Node\Stmt\TraitUse::class:
             $this->nodes['traits'][$node->traits[0]->parts[0]] = $node;
             break;
           case \PhpParser\Node\Stmt\Function_::class:
-            $this->nodes['functions'][$node->name] = $node;
+            $this->nodes['functions'][$node->name->toString()] = $node;
             break;
           case \PhpParser\Node\Stmt\ClassMethod::class:
-            $this->nodes['methods'][$node->name] = $node;
+            $this->nodes['methods'][$node->name->toString()] = $node;
             break;
         }
       }
