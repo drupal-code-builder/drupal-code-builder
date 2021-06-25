c94e4479732f6b07fd6da2b959d49e2ea0734dbd
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2021-06-25 06:53:21 +0100

    Removed obsolete method.

--- Definition/GeneratorDefinition.php
+++ Definition/GeneratorDefinition.php
@@ -55,21 +55,6 @@ class GeneratorDefinition extends PropertyDefinition {
     return new static($data_type, $generator_type);
   }
 
-  // we need a separate method for this because TESTS definitely don't want to
-  // go adding the converted array defs.
-  // also so we can remove this functionality easily in future
-  static public function createFromGeneratorTypeWithConversion(string $generator_type, string $data_type = 'complex'): self {
-    $definition = new static($data_type, $generator_type);
-
-    $generate_task = \DrupalCodeBuilder\Factory::getTask('Generate', 'module');
-    // ARGH we're calling a method meant for the ROOT only!
-    $array_property_info = $generate_task->getComponentDataInfo($generator_type, TRUE);
-
-    \DrupalCodeBuilder\Generator\BaseGenerator::addArrayPropertyInfoToDefinition($definition, $array_property_info);
-
-    return $definition;
-  }
-
   /**
    * Gets the component type for this property.
    *
