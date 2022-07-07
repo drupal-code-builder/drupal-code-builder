f9889515fec9339f523d7777f0aaa175fee363e0
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-07-07 16:22:31 +0100

    Fixed missing docs.

--- Test/Fixtures/File/MockableExtension.php
+++ Test/Fixtures/File/MockableExtension.php
@@ -4,14 +4,33 @@ namespace DrupalCodeBuilder\Test\Fixtures\File;
 
 use DrupalCodeBuilder\File\DrupalExtension;
 
+/**
+ * Mock for an existing Drupal extension in tests.
+ */
 class MockableExtension extends DrupalExtension {
 
   protected $mockedFiles = [];
 
+  /**
+   * Set mocked code.
+   *
+   * @param string $relative_file_path
+   *   The relative file path. This MUST use tokens such as '%module' instead of
+   *   the extension name, as this class doesn't call getRealPath(), and can't
+   *   because the name of the fixture module folder and the mocked module
+   *   file data may not match.
+   *   TODO: fix this.
+   *
+   * @param string $content
+   *   The file contents.
+   */
   public function setFile(string $relative_file_path, string $content) {
     $this->mockedFiles[$relative_file_path] = $content;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function hasFile(string $relative_file_path): bool {
     if (isset($this->mockedFiles[$relative_file_path])) {
       return TRUE;
@@ -21,6 +40,9 @@ class MockableExtension extends DrupalExtension {
     }
   }
 
+  /**
+   * {@inheritdoc}
+   */
   protected function getFileContents($relative_file_path) {
     if (isset($this->mockedFiles[$relative_file_path])) {
       return $this->mockedFiles[$relative_file_path];
