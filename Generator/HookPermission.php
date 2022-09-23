be1db5ec5c858ba840e0444f7be40dd986a7b6e0
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-09-23 16:13:43 +0100

    Changed Permission7 to return contents.

--- Generator/HookPermission.php
+++ Generator/HookPermission.php
@@ -26,14 +26,14 @@ class HookPermission extends HookImplementation {
   protected function buildComponentContents($children_contents) {
     // If we have no children, i.e. no Permission components, then hand over to
     // the parent, which will output the default hook code.
-    if (empty($children_contents)) {
+    if (empty($this->containedComponents)) {
       return parent::buildComponentContents($children_contents);
     }
 
     $code = [];
     $code[] = '£permissions = array();';
-    foreach ($this->filterComponentContentsForRole($children_contents, 'item') as $menu_item_lines) {
-      $code = array_merge($code, $menu_item_lines);
+    foreach ($this->containedComponents as $key => $child_item) {
+      $code = array_merge($code, $child_item->getContents());
     }
     $code[] = '';
     $code[] = 'return £permissions;';
