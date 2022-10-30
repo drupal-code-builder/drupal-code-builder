8a06197a016749211c444db7121218df0492b84c
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-10-30 10:23:20 +0000

    Added assertion.

--- MutableTypedData/Data/MergeableComplexDataTrait.php
+++ MutableTypedData/Data/MergeableComplexDataTrait.php
@@ -17,6 +17,10 @@ trait MergeableComplexDataTrait {
 
     $other_items = $other->items();
     foreach ($other_items as $property_name => $other_item) {
+      // Hacky guard against using class property names as data property names.
+      // See https://github.com/joachim-n/mutable-typed-data/issues/10.
+      assert(is_object($this->{$property_name}));
+
       $result[] = $this->{$property_name}->merge($other->{$property_name});
     }
 
