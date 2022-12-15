534c81efc8ae75bbe353a3ba77af39985602f59f
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-12-15 12:59:59 +0000

    Fixed repetition of ‘service’ in label for test mocked service comment. Fixes #279.

--- Generator/PHPUnitTest.php
+++ Generator/PHPUnitTest.php
@@ -375,7 +375,7 @@ class PHPUnitTest extends PHPClassFile {
     // Mocked services.
     if (!empty($this->getContentsElement('service_mocked'))) {
       foreach ($this->getContentsElement('service_mocked') as $service_info) {
-        $setup_lines[] = "// Mock the {$service_info['label']} service.";
+        $setup_lines[] = "// Mock the {$service_info['label']}.";
         $setup_lines[] = "£{$service_info['variable_name']} = £this->prophesize({$service_info['typehint']}::class);";
         $setup_lines[] = "£this->container->set('{$service_info['id']}', £{$service_info['variable_name']}->reveal());";
       }
