bd93c01c2ee58aedd487b6bb2633f9a95886c5b2
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-05-14 18:36:15 +0100

    Fixed missing docs.

--- Definition/OptionsProviderInterface.php
+++ Definition/OptionsProviderInterface.php
@@ -7,6 +7,12 @@ namespace DrupalCodeBuilder\Definition;
  */
 interface OptionsProviderInterface {
 
+  /**
+   * Provides options for a property.
+   *
+   * @return \MutableTypedData\Definition\OptionDefinition[]
+   *   An array of option definitions, keyed by the option ID.
+   */
   public function getOptions(): array;
 
 }
