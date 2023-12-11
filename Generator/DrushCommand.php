0fe4f6f7bb31ee72b0deb70b8841a55e68e1d83d
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-12-11 22:49:05 +0000

    fix

--- Generator/DrushCommand.php
+++ Generator/DrushCommand.php
@@ -3,6 +3,7 @@
 namespace DrupalCodeBuilder\Generator;
 
 use CaseConverter\CaseString;
+use DrupalCodeBuilder\Definition\GeneratorDefinition;
 use DrupalCodeBuilder\Definition\PresetDefinition;
 use DrupalCodeBuilder\Definition\PropertyDefinition;
 use MutableTypedData\Definition\DefaultDefinition;
