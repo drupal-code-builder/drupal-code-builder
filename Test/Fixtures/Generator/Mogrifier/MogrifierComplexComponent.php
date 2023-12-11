a9a0ab968edc4735289226c07b72e2b4e5250aba
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-12-11 07:16:08 +0000

    clean

--- Test/Fixtures/Generator/Mogrifier/MogrifierComplexComponent.php
+++ Test/Fixtures/Generator/Mogrifier/MogrifierComplexComponent.php
@@ -8,15 +8,6 @@ use DrupalCodeBuilder\Definition\PropertyDefinition;
 
 class MogrifierComplexComponent extends BaseGenerator {
 
-  // // TODO rename to  getGeneratorDataDefinition() when that is removed?
-  // // KILL??
-  // function getDataDefinition() {
-  //   return PropertyDefinition::create('complex')
-  //     ->setProperties([
-
-  //     ]);
-  // }
-
   public static function addProperties(PropertyDefinition $definition) {
     $definition->addProperties([
       'string_property' => PropertyDefinition::create('string'),
