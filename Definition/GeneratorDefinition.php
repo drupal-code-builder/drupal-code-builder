279b75df50b78c2e33fcebcb48749d3530ccebcf
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-12-11 07:16:11 +0000

    docs

--- Definition/GeneratorDefinition.php
+++ Definition/GeneratorDefinition.php
@@ -5,12 +5,12 @@ namespace DrupalCodeBuilder\Definition;
 /**
  * Defines a data property that has an associated generator.
  *
- * TODO: aaargh! Is this to be used in the parent generator, or the generator
- * itself?? There are examples of both usages!! Plugin and PluginType use in the
- * generator itself; BaseGenerator in its conversion shim code is the parent
- * using it to define the child item. So far, the correct thing is to use
- * BaseGenerator::getLazyDataDefinitionForGeneratorType() in the parent
- * generator case.
+ * This gets the data type from the generator class, and lazily allows the
+ * geneator class to add to the definition.
+ *
+ * The laziness is so that generator classes can be used repeatedly and take
+ * care of removing properties from a parent class that would cause recursion:
+ * for example the Module and TestModule generators.
  */
 class GeneratorDefinition extends PropertyDefinition {
 
