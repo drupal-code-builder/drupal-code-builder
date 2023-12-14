9ec7266c30446e65a4edeaad0346d104d8983acf
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-12-14 10:29:45 +0000

    unsubclass

--- Definition/DeferredGeneratorDefinition.php
+++ Definition/DeferredGeneratorDefinition.php
@@ -5,14 +5,31 @@ namespace DrupalCodeBuilder\Definition;
 /**
  * Defines a data property that has an associated generator.
  *
+ * The data definition from the generator is not merged into this definition.
+ * This is used for simple properties for which a generator component is
+ * needed. The component is instantiated as standalone data during component
+ * collection.
  *
- *
- * TODO Rename this - it's not about simple, it's about proxied - the property
- * in the parent is not the same as the generator's properties.
- *
- * TODO don't subclass MergingGeneratorDefinition, it's different.
+ * For example, API, Readme, ServiceProvider, where a boolean property on the
+ * host data suffices, but a generator is used to create the code.
  */
-class DeferredGeneratorDefinition extends MergingGeneratorDefinition {
+class DeferredGeneratorDefinition extends PropertyDefinition {
+
+  /**
+   * Constructor.
+   *
+   * @param string $data_type NO!
+   *   The data type.
+   * @param string $generator_type
+   *   The generator type.
+   */
+  public function __construct(
+    string $data_type,
+    protected string $componentType,
+    protected string $generatorClass,
+  ) {
+    parent::__construct($data_type);
+  }
 
   /**
    * Creates a new definition from a component type.
@@ -35,8 +52,14 @@ class DeferredGeneratorDefinition extends MergingGeneratorDefinition {
     return new static($data_type, $generator_type, $generator_class);
   }
 
-  public function getProperties() {
-    return [];
+  /**
+   * Gets the component type for this property.
+   *
+   * @return string
+   *   The component type.
+   */
+  public function getComponentType() :string {
+    return $this->componentType;
   }
 
 }
