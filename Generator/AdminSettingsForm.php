c82873cb2548fa87637bddebd258939aed33dfd3
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-12-14 13:42:36 +0000

    wip

--- Generator/AdminSettingsForm.php
+++ Generator/AdminSettingsForm.php
@@ -2,6 +2,7 @@
 
 namespace DrupalCodeBuilder\Generator;
 
+use DrupalCodeBuilder\Definition\PropertyListInterface;
 use DrupalCodeBuilder\Utility\InsertArray;
 use MutableTypedData\Definition\DefaultDefinition;
 use DrupalCodeBuilder\Definition\PropertyDefinition;
@@ -15,7 +16,7 @@ class AdminSettingsForm extends Form {
   /**
    * {@inheritdoc}
    */
-  public static function addToGeneratorDefinition($definition) {
+  public static function addToGeneratorDefinition(PropertyListInterface $definition) {
     parent::addToGeneratorDefinition($definition);
 
     // Because this component is declared in the Module root component, we
