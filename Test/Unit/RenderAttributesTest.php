c86cb1c4922fa20717b8f224d8df96424d615f27
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-01-13 22:15:17 +0000

    Added renderer for PHP attributes.

---
+++ Test/Unit/RenderAttributesTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace DrupalCodeBuilder\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+use DrupalCodeBuilder\Generator\Render\PhpAttributes;
+
+/**
+ * Unit tests for the attribute renderer.
+ */
+class RenderAttributesTest extends TestCase {
+
+  /**
+   * Tests an annotation with nested annotations.
+   */
+  public function testAttributeWithNesting() {
+    $attribute = PhpAttributes::class(
+      'Drupal\Core\Block\Attribute\Block',
+      [
+        'id' => 'cat',
+        'admin_label' => PhpAttributes::object(
+          'Drupal\Core\StringTranslation\TranslatableMarkup',
+          "Miaow",
+        ),
+        'extra' => [
+          'purr' => 'value',
+        ],
+      ],
+    );
+
+    $lines = $attribute->render();
+    $attribute = implode("\n", $lines);
+
+    $expected_attribute = <<<EOT
+    #[\Drupal\Core\Block\Attribute\Block(
+      id: "cat",
+      admin_label: new \Drupal\Core\StringTranslation\TranslatableMarkup("Miaow"),
+      extra: [
+        'purr' => "value",
+      ],
+    )]
+    EOT;
+
+    $this->assertEquals($expected_attribute, $attribute);
+  }
+
+}
