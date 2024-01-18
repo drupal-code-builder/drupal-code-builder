28ab9ae056b108a8fb83bc6f0410b684b161aa71
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-01-18 10:29:34 +0000

    Changed HooksCollector classes so high version is the default.

--- Task/Collect/HooksCollector9.php
+++ Task/Collect/HooksCollector9.php
@@ -7,8 +7,8 @@ use DrupalCodeBuilder\Environment\EnvironmentInterface;
 /**
  * Task helper for collecting data on hooks on Drupal 9.
  */
-class HooksCollector9 extends HooksCollector8 {
+class HooksCollector9 extends HooksCollector10 {
 
-  // Identical to Drupal 8.
+  // Identical to Drupal 10.
 
 }
