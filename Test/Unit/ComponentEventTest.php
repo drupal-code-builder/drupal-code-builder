39356e9026881c69aeee2c3a3955e9a73f876625
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-04-23 17:24:28 +0100

    Test

---
+++ Test/Unit/ComponentEventTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace DrupalCodeBuilder\Test\Unit;
+
+use DrupalCodeBuilder\Test\Fixtures\File\MockableExtension;
+use DrupalCodeBuilder\Test\Unit\Parsing\PHPTester;
+use DrupalCodeBuilder\Test\Unit\Parsing\YamlTester;
+use MutableTypedData\Exception\InvalidInputException;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Tests for event component.
+ */
+class ComponentEventTest extends TestBase {
+
+  /**
+   * The Drupal core major version to set up for this test.
+   *
+   * @var int
+   */
+  protected $drupalMajorVersion = 10;
+
+  /**
+   * Test generating an event.
+   */
+  public function testEventGeneration() {
+    // Assemble module data.
+    $module_name = 'test_module';
+    $module_data = [
+      'base' => 'module',
+      'root_name' => $module_name,
+      'readable_name' => 'Test Module',
+      'short_description' => 'Test Module description',
+      'events' => [
+        0 => [
+          'event_constant' => 'MOO',
+          'event_value' => 'moo',
+        ],
+      ],
+      'readme' => FALSE,
+    ];
+
+    $files = $this->generateModuleFiles($module_data);
+
+    $this->assertFiles([
+      'test_module.info.yml',
+      "src/Event/TestModuleEvents.php",
+    ], $files);
+
+    $event_constants_file = $files['src/Event/TestModuleEvents.php'];
+    dump($event_constants_file);
+  }
+
+}
