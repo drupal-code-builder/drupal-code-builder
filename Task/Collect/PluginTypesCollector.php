456c770936b161beec8586f7a0893bf8af861f35
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-06-11 17:48:09 +0100

    Added support for ReflectionUnionType in plugin type analysis.

--- Task/Collect/PluginTypesCollector.php
+++ Task/Collect/PluginTypesCollector.php
@@ -665,22 +665,41 @@ class PluginTypesCollector extends CollectorBase  {
 
       $type_reflection = $property_reflection->getType();
       if ($type_reflection) {
-        // Assume that there aren't any compound types for now!
-        assert($type_reflection instanceof \ReflectionNamedType);
-
-        $type = $type_reflection->getName();
-
-        // Prefix a class with a '\'. We can assume that class types start with
-        // an uppercase letter, and that there are no static / self etc types.
-        if (ctype_upper(substr($type, 0, 1))) {
-          $type = '\\' . $type;
-        }
-
-        $annotation_property_data['type'] = $type;
+        $annotation_property_data['type'] = $this->getTypeStringFromReflection($type_reflection);
       }
 
       $data['plugin_properties'][$property_reflection->name] = $annotation_property_data;
     }
+  }
+
+  /**
+   * Produces the string representation of type from the ReflectionType.
+   *
+   * @param \ReflectionType $type_reflection
+   *   The reflection type. If this is a compound type, this method calls itself
+   *   with each of the subtypes.
+   *
+   * @return string
+   *   The the string representation of type, usable in code.
+   */
+  protected function getTypeStringFromReflection(\ReflectionType $type_reflection): string {
+    if ($type_reflection instanceof \ReflectionNamedType) {
+      $type = $type_reflection->getName();
+
+      // Prefix a class with a '\'. We can assume that class types start with
+      // an uppercase letter, and that there are no static / self etc types.
+      if (ctype_upper(substr($type, 0, 1))) {
+        $type = '\\' . $type;
+      }
+
+      return $type;
+    }
+    elseif ($type_reflection instanceof \ReflectionUnionType) {
+      $union_types = array_map(fn (\ReflectionType $map_type) => $this->getTypeStringFromReflection($map_type), $type_reflection->getTypes());
+      return implode('|', $union_types);
+    }
+
+    assert(FALSE, 'Unsupported ReflectionType class.');
   }
 
   /**
