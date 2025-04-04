9fb1e41b4d1977db96734ac286d92d3e7f7ac588
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2025-04-04 11:31:01 +0100

    Updated test sample data.

--- Test/sample_hook_definitions/11/element_types_processed.php
+++ Test/sample_hook_definitions/11/element_types_processed.php
@@ -5,17 +5,23 @@ array (
     'type' => 'machine_name',
     'label' => 'machine_name',
     'form' => true,
+    'class_filepath' => 'core/lib/Drupal/Core/Render/Element/MachineName.php',
+    'description' => 'Provides a machine name render element.',
   ),
   'textarea' => 
   array (
     'type' => 'textarea',
     'label' => 'textarea',
     'form' => true,
+    'class_filepath' => 'core/lib/Drupal/Core/Render/Element/Textarea.php',
+    'description' => 'Provides a form element for input of multiple-line text.',
   ),
   'textfield' => 
   array (
     'type' => 'textfield',
     'label' => 'textfield',
     'form' => true,
+    'class_filepath' => 'core/lib/Drupal/Core/Render/Element/Textfield.php',
+    'description' => 'Provides a one-line text field form element.',
   ),
 );
